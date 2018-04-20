0643ee70f12329f7eff0bd4f652985d792369d60
XintaoWang <wxt1994@126.com>
2018-04-20 16:40:36 +0800

    add Degradation Net
    Former-commit-id: b9c98e0a81ba43dc1e9092ba868218be991b505f

---
+++ codes/data_preprocess/extract_subimgs_single.py
@@ -0,0 +1,71 @@
+import os
+import os.path
+import cv2
+import numpy as np
+from multiprocessing import Pool
+import time
+
+def worker(GT_paths, save_GT_dir):
+    crop_sz = 500
+    step  = 400
+    thres_sz = 100
+    for GT_path in GT_paths:
+        base_name = os.path.basename(GT_path)
+        print(base_name, os.getpid())
+        img_GT = cv2.imread(GT_path, cv2.IMREAD_UNCHANGED)
+
+        n_channels = len(img_GT.shape)
+        if n_channels == 2:
+            h, w = img_GT.shape
+        elif n_channels == 3:
+            h, w, c = img_GT.shape
+        else:
+            raise ValueError('Wrong image shape - {}'.format(n_channels))
+
+        h_space = np.arange(0, h-crop_sz+1, step)
+        if h - (h_space[-1] + crop_sz) > thres_sz:
+            h_space = np.append(h_space, h-crop_sz)
+        w_space = np.arange(0, w-crop_sz+1, step)
+        if w - (w_space[-1] + crop_sz) > thres_sz:
+            w_space = np.append(w_space, w-crop_sz)
+        index = 0
+        for x in h_space:
+            for y in w_space:
+                index += 1
+                if n_channels == 2:
+                    crop_img = img_GT[x:x+crop_sz, y:y+crop_sz]
+                else:
+                    crop_img = img_GT[x:x+crop_sz, y:y+crop_sz, :]
+
+                crop_img = np.ascontiguousarray(crop_img)
+                index_str = '{:03d}'.format(index)
+                cv2.imwrite(os.path.join(save_GT_dir, base_name.replace('.png', '_s'+index_str+'.png')), crop_img, \
+                            [cv2.IMWRITE_PNG_COMPRESSION, 0])
+
+if __name__=='__main__':
+
+    GT_dir = '/mnt/SSD/xtwang/BasicSR_datasets/GOPRO/test/blur_gamma'
+    save_GT_dir = '/mnt/SSD/xtwang/BasicSR_datasets/GOPRO/test/blur_gamma_sub'
+    n_thread = 20
+
+    print('Parent process %s.' % os.getpid())
+    start = time.time()
+
+    p = Pool(n_thread)
+    # read all files to a list
+    all_files = []
+    for root, _, fnames in sorted(os.walk(GT_dir)):
+        full_path = [os.path.join(root, x) for x in fnames]
+        all_files.extend(full_path)
+    # cut into subtasks
+    def chunkify(lst, n): # for non-continuous chunks
+        return [lst[i::n] for i in range(n)]
+    sub_lists = chunkify(all_files, n_thread)
+    # call workers
+    for i in range(n_thread):
+        p.apply_async(worker, args=(sub_lists[i], save_GT_dir))
+    print('Waiting for all subprocesses done...')
+    p.close()
+    p.join()
+    end = time.time()
+    print('All subprocesses done. Using time {} sec.'.format(end - start))
--- codes/models/modules/generator.py
+++ codes/models/modules/generator.py
@@ -35,3 +35,29 @@ class SRResNet_torch(nn.Module):
     def forward(self, x):
         x = self.model(x)
         return x
+
+
+class DegradationNet(nn.Module):
+    def __init__(self, in_nc, out_nc, nf, nb, upscale=4, norm_type='batch', act_type='relu', \
+            mode='NAC', res_scale=1, upsample_mode='upconv'):
+        super(DegradationNet, self).__init__()
+        n_upscale = int(math.sqrt(upscale))
+
+        fea_conv = B.conv_block(in_nc, nf, kernel_size=3, norm_type=None, act_type=None)
+        resnet_blocks_0 = [B.ResNetBlock(nf, nf, nf, norm_type=norm_type, act_type=act_type,\
+            mode=mode, res_scale=res_scale) for _ in range(2)]
+        max_pooling_0 = nn.MaxPool2d(3, stride=2, padding=1)
+        resnet_blocks_1 = [B.ResNetBlock(nf, nf, nf, norm_type=norm_type, act_type=act_type,\
+            mode=mode, res_scale=res_scale) for _ in range(2)]
+        max_pooling_1 = nn.MaxPool2d(3, stride=2, padding=1)
+        resnet_blocks_2 = [B.ResNetBlock(nf, nf, nf, norm_type=norm_type, act_type=act_type,\
+            mode=mode, res_scale=res_scale) for _ in range(14)]
+        middle_conv = B.conv_block(nf, nf, kernel_size=3, norm_type=None, act_type=None)
+        reduce_conv = B.conv_block(nf, out_nc, kernel_size=3, norm_type=None, act_type=None)
+
+        self.model = B.sequential(fea_conv, *resnet_blocks_0, max_pooling_0, *resnet_blocks_1, \
+            max_pooling_1, middle_conv, B.ShortcutBlock(B.sequential(*resnet_blocks_2)), reduce_conv)
+
+    def forward(self, x):
+        x = self.model(x)
+        return x
