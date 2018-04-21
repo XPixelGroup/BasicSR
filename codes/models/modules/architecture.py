3e0882675758d2c45cf49277c5cb9b315ac16d8d
XintaoWang <wxt1994@126.com>
2018-04-21 17:53:04 +0800

    update discriminator and vggfeatureextractor network

--- codes/models/modules/architecture.py
+++ codes/models/modules/architecture.py
@@ -74,7 +74,7 @@ class DegradationNet(nn.Module):
 ####################
 # VGG style Discriminator with input size 128*128
 class Discriminaotr_VGG_128(nn.Module):
-    def __init__(self, in_nc, out_nc, base_nf, norm_type='batch', act_type='leakyrelu', mode='CNA'):
+    def __init__(self, in_nc, base_nf, norm_type='batch', act_type='leakyrelu', mode='CNA'):
         super(Discriminaotr_VGG_128, self).__init__()
         # features
         # hxw, c
@@ -83,17 +83,17 @@ class Discriminaotr_VGG_128(nn.Module):
             mode=mode)
         conv1 = B.conv_block(base_nf, base_nf, kernel_size=4, stride=2, norm_type=norm_type, \
             act_type=act_type, mode=mode)
-        # 64, 128
+        # 64, 64
         conv2 = B.conv_block(base_nf, base_nf*2, kernel_size=3, stride=1, norm_type=norm_type, \
             act_type=act_type, mode=mode)
         conv3 = B.conv_block(base_nf*2, base_nf*2, kernel_size=4, stride=2, norm_type=norm_type, \
             act_type=act_type, mode=mode)
-        # 32, 256
+        # 32, 128
         conv4 = B.conv_block(base_nf*2, base_nf*4, kernel_size=3, stride=1, norm_type=norm_type, \
             act_type=act_type, mode=mode)
         conv5 = B.conv_block(base_nf*4, base_nf*4, kernel_size=4, stride=2, norm_type=norm_type, \
             act_type=act_type, mode=mode)
-        # 16, 512
+        # 16, 256
         conv6 = B.conv_block(base_nf*4, base_nf*8, kernel_size=3, stride=1, norm_type=norm_type, \
             act_type=act_type, mode=mode)
         conv7 = B.conv_block(base_nf*8, base_nf*8, kernel_size=4, stride=2, norm_type=norm_type, \
@@ -103,7 +103,7 @@ class Discriminaotr_VGG_128(nn.Module):
             act_type=act_type, mode=mode)
         conv9 = B.conv_block(base_nf*8, base_nf*8, kernel_size=4, stride=2, norm_type=norm_type, \
             act_type=act_type, mode=mode)
-        # 4, 256
+        # 4, 512
         self.features = B.sequential(conv0, conv1, conv2, conv3, conv4, conv5, conv6, conv7, conv8,\
             conv9)
 
@@ -124,9 +124,9 @@ class Discriminaotr_VGG_128(nn.Module):
 ####################
 # Perceptual Network
 ####################
-# Assume input in range [0, 1]
+# Assume input range is [0, 1]
 class VGGFeatureExtractor(nn.Module):
-    def __init__(self, feature_layer=11, use_bn=True, use_input_norm=True):
+    def __init__(self, feature_layer=34, use_bn=False, use_input_norm=True, tensor=torch.FloatTensor):
         super(VGGFeatureExtractor, self).__init__()
         if use_bn:
             model = torchvision.models.vgg19_bn(pretrained=True)
@@ -134,14 +134,14 @@ class VGGFeatureExtractor(nn.Module):
             model = torchvision.models.vgg19(pretrained=True)
         self.use_input_norm = use_input_norm
         if self.use_input_norm:
-            mean = Variable(torch.Tensor([0.485, 0.456, 0.406]).view(1,3,1,1), requires_grad=False)
+            mean = Variable(tensor([0.485, 0.456, 0.406]).view(1,3,1,1), requires_grad=False)
             # [0.485-1, 0.456-1, 0.406-1] if input in range [-1,1]
-            std = Variable(torch.Tensor([0.229, 0.224, 0.225]).view(1,3,1,1), requires_grad=False)
+            std = Variable(tensor([0.229, 0.224, 0.225]).view(1,3,1,1), requires_grad=False)
             # [0.229*2, 0.224*2, 0.225*2] if input in range [-1,1]
             self.register_buffer('mean', mean)
             self.register_buffer('std', std)
         self.features = nn.Sequential(*list(model.features.children())[:(feature_layer + 1)])
-        # No need to bp to variable
+        # No need to BP to variable
         for k, v in self.features.named_parameters():
             v.requires_grad = False
 
