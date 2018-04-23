857af45015991cbbea3ce32d67a5fc9bf7adafee
XintaoWang <wxt1994@126.com>
2018-04-23 16:57:49 +0800

    add LRHRRef dataloader

--- codes/test_dataloader.py
+++ codes/test_dataloader.py
@@ -15,18 +15,19 @@ opt = {}
 # opt['subset_file'] = None
 
 # HR path and LR path
-opt['name'] = 'DIV2K800'
-opt['dataroot_HR'] = '/mnt/SSD/xtwang/BasicSR/DIV2K800_sub'
-opt['dataroot_LR'] = '/mnt/SSD/xtwang/BasicSR/DIV2K800_sub_bic'
+opt['name'] = 'DIV2K'
+opt['dataroot_HR'] = '/mnt/SSD/xtwang/BasicSR_datasets/DIV2K800/DIV2K800_sub'
+opt['dataroot_LR'] = '/mnt/SSD/xtwang/BasicSR_datasets/DIV2K800/DIV2K800_sub_bicLR'
 opt['subset_file'] = None
-
+opt['dataroot_ref'] = '/mnt/SSD/xtwang/BasicSR_datasets/DIV2K800/DIV2K800_sub'
+opt['reverse'] = False
 opt['data_type'] = 'img'
-opt['mode'] = 'LRHR_pair'
+opt['mode'] = 'LRHRref'
 opt['phase'] = 'train'
 opt['use_shuffle'] = True
 opt['n_workers'] = 3
 opt['batch_size'] = 64
-opt['HR_size'] = 192
+opt['HR_size'] = 128
 opt['scale'] = 4
 opt['use_flip'] = True
 opt['use_rot'] = True
@@ -38,5 +39,7 @@ for i, data in enumerate(train_loader):
     print(i)
     LR = data['LR']
     HR = data['HR']
+    ref = data['ref']
     torchvision.utils.save_image(LR, 'LR_{:03d}.png'.format(i), nrow=8, padding=2, normalize=False)
     torchvision.utils.save_image(HR, 'HR_{:03d}.png'.format(i), nrow=8, padding=2, normalize=False)
+    torchvision.utils.save_image(ref, 'ref_{:03d}.png'.format(i), nrow=8, padding=2, normalize=False)
