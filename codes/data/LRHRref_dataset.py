94a57bcc540e65e42aab646da7c0d5b119627453
XintaoWang <wxt1994@126.com>
2018-04-26 17:25:45 +0800

    clean data loader

--- codes/data/LRHRref_dataset.py
+++ codes/data/LRHRref_dataset.py
@@ -16,7 +16,6 @@ class LRHRRefDataset(data.Dataset):
         self.paths_LR = []
         self.paths_HR = []
         self.paths_ref = []
-        assert opt['phase'] == 'train'
 
         if opt['phase'] == 'train' and opt['subset_file'] is not None:
             # get HR image paths from list
@@ -24,31 +23,30 @@ class LRHRRefDataset(data.Dataset):
                 self.paths_HR = sorted([os.path.join(opt['dataroot_HR'], line.rstrip('\n')) \
                         for line in f])
             if opt['dataroot_LR'] is not None:
-                raise NotImplementedError('subset only support generating LR now.')
+                raise NotImplementedError('Now subset only support generating LR on-the-fly.')
         else:
             if opt['dataroot_LR'] is not None:
                 self.paths_LR = sorted(get_image_paths(opt['dataroot_LR']))
             if opt['dataroot_HR'] is not None:
                 self.paths_HR = sorted(get_image_paths(opt['dataroot_HR']))
-            assert self.paths_LR or self.paths_HR, 'both LR and HR paths are empty.'
+            assert self.paths_LR or self.paths_HR, 'Both LR and HR paths are empty.'
             if self.paths_LR and self.paths_HR:
-                assert len(self.paths_LR) == len(self.paths_HR), 'HR and LR datasets are not the same.'
-
+                assert len(self.paths_LR) == len(self.paths_HR), \
+                    'HR and LR datasets have different number of images.'
         # ref images
-        self.paths_ref = sorted(get_image_paths(opt['dataroot_ref']))
-        self.n_ref = len(self.paths_ref)
+        if opt['dataroot_ref']:
+            self.paths_ref = sorted(get_image_paths(opt['dataroot_ref']))
+            self.n_ref = len(self.paths_ref)
 
         if opt['data_type'] == 'bin':
-            # only in train, not val and test
+            # only used in train TODO
             self.HR_bin_list = []
             self.LR_bin_list = []
-            # TODO
 
-        self.aug_scale_list = [] # TODO may need to incorporate into the option, now it is useless
+        # self.aug_scale_list = [] # may need to incorporate into the option, now it is useless
 
     def __getitem__(self, index):
-        HR_path, LR_path = None, None
-        img_ref = None
+        HR_path, LR_path, ref_path = None, None, None
         scale = self.opt['scale']
         if self.paths_HR:
             # get HR image
@@ -60,7 +58,6 @@ class LRHRRefDataset(data.Dataset):
             img_HR = img_HR * 1.0 / 255 # numpy.ndarray(float64), [0,1], HWC, BGR
             if img_HR.ndim == 2: # gray image, add one dimension
                 img_HR = np.expand_dims(img_HR, axis=2)
-
             # get LR image
             if self.paths_LR:
                 if self.opt['data_type'] == 'img':
@@ -68,58 +65,33 @@ class LRHRRefDataset(data.Dataset):
                     img_LR = cv2.imread(LR_path, cv2.IMREAD_UNCHANGED)
                 else: # bin
                     img_LR = self.LR_bin_list[index]
-                img_LR = img_LR * 1.0 / 255  # numpy.ndarray(float64), [0,1], HWC, BGR
-                if img_LR.ndim == 2:  # gray image, add one dimension
+                img_LR = img_LR * 1.0 / 255
+                if img_LR.ndim == 2:  # gray image
                     img_LR = np.expand_dims(img_LR, axis=2)
             else: # down-sampling on-the-fly
                 h_HR, w_HR, _ = img_HR.shape
-                # using bilinear now
-                img_LR = cv2.resize(img_HR, (w_HR//scale, h_HR//scale), interpolation=cv2.INTER_LINEAR)
+                # using INTER_LINEAR now
+                img_LR = cv2.resize(img_HR, (w_HR//scale, h_HR//scale), \
+                    interpolation=cv2.INTER_LINEAR)
         else: # only read LR image in test phase
             LR_path = self.paths_LR[index]
             img_LR = cv2.imread(LR_path, cv2.IMREAD_UNCHANGED)
             img_LR = img_LR * 1.0 / 255
             if img_LR.ndim == 2:
                 img_LR = np.expand_dims(img_LR, axis=2)
-
         H, W, C = img_LR.shape
 
         if self.opt['phase'] == 'train':
             HR_size = self.opt['HR_size']
             LR_size = HR_size // scale
 
-            # randomly scale
-            # if self.aug_scale_list != []:
-            #     rnd_scale = random.choice(self.scale_list)
-            #     H, W = int(rnd_scale*H), int(rnd_scale*W)
-            #     if H < LR_size:
-            #         H = LR_size
-            #     if W < LR_size:
-            #         W = LR_size
-            #     img_LR = cv2.resize(img_LR, (W,H), interpolation=cv2.INTER_LINEAR)
-            #     img_HR = cv2.resize(img_HR, (W*self.opt.scale,H*self.opt.scale), \
-            #           interpolation=cv2.INTER_LINEAR)
-
-            # get ref image
-            ref_idx = random.randint(0, self.n_ref-1)
-            ref_path = self.paths_ref[ref_idx]
-            img_ref = cv2.imread(ref_path, cv2.IMREAD_UNCHANGED)
-            img_ref = img_ref * 1.0 / 255
             # randomly crop
             rnd_h = random.randint(0, max(0, H-LR_size))
             rnd_w = random.randint(0, max(0, W-LR_size))
             img_LR = img_LR[rnd_h:rnd_h+LR_size, rnd_w:rnd_w+LR_size, :]
-
             rnd_h_HR, rnd_w_HR = rnd_h*scale, rnd_w*scale
             img_HR = img_HR[rnd_h_HR:rnd_h_HR+HR_size, rnd_w_HR:rnd_w_HR+HR_size, :]
-
-            # ref
-            ref_size = LR_size if self.opt['reverse'] else HR_size
-            rnd_h = random.randint(0, max(0, img_ref.shape[0] - ref_size))
-            rnd_w = random.randint(0, max(0, img_ref.shape[1] - ref_size))
-            img_ref = img_ref[rnd_h:rnd_h + ref_size, rnd_w:rnd_w + ref_size, :]
             img_LR = np.ascontiguousarray(img_LR)
-            img_ref = np.ascontiguousarray(img_ref)
             img_HR = np.ascontiguousarray(img_HR)
 
             # augmentation - flip, rotation
@@ -133,24 +105,39 @@ class LRHRRefDataset(data.Dataset):
                 return img
             img_HR = _augment(img_HR)
             img_LR = _augment(img_LR)
-            img_ref = _augment(img_ref)
-            img_ref = torch.from_numpy(np.transpose(img_ref[:, :, [2, 1, 0]], (2, 0, 1))).float()
+
+            # ref images
+            if self.opt['dataroot_ref']:
+                ref_idx = random.randint(0, self.n_ref - 1)
+                ref_path = self.paths_ref[ref_idx]
+                img_ref = cv2.imread(ref_path, cv2.IMREAD_UNCHANGED)
+                img_ref = img_ref * 1.0 / 255
+                if img_ref.ndim == 2:
+                    img_ref = np.expand_dims(img_ref, axis=2)
+                ref_size = LR_size if self.opt['reverse'] else HR_size
+                rnd_h = random.randint(0, max(0, img_ref.shape[0] - ref_size))
+                rnd_w = random.randint(0, max(0, img_ref.shape[1] - ref_size))
+                img_ref = img_ref[rnd_h:rnd_h + ref_size, rnd_w:rnd_w + ref_size, :]
+                img_ref = np.ascontiguousarray(img_ref)
+                img_ref = _augment(img_ref)
+                img_ref = torch.from_numpy(np.transpose(img_ref[:, :, [2, 1, 0]], (2, 0, 1))).float()
 
         # numpy to tensor, HWC to CHW, BGR to RGB
-        if self.paths_HR:
+        if HR_path:
             img_HR = torch.from_numpy(np.transpose(img_HR[:, :, [2,1,0]], (2, 0, 1))).float()
         img_LR = torch.from_numpy(np.transpose(img_LR[:, :, [2,1,0]], (2, 0, 1))).float()
 
-
-        if not self.paths_HR: # read only LR image in test phase
+        if not HR_path:  # read only LR image in test phase
             return {'LR': img_LR, 'LR_path': LR_path}
-        elif  LR_path is None:
-            LR_path = HR_path
-
+        elif LR_path is None:
+            LR_path = HR_path # tricky
         if self.opt['reverse']:
             img_LR, img_HR = img_HR, img_LR
             LR_path, HR_path = HR_path, LR_path
-        return {'LR': img_LR, 'HR': img_HR, 'ref':img_ref, 'LR_path': LR_path, 'HR_path': HR_path}
+        return_dict = {'LR': img_LR, 'HR': img_HR, 'LR_path': LR_path, 'HR_path': HR_path}
+        if ref_path:
+            return_dict['ref'] = img_ref
+        return return_dict
 
     def __len__(self):
         if self.paths_LR:
--- codes/test_dataloader.py
+++ codes/test_dataloader.py
@@ -1,26 +1,23 @@
 import torchvision.utils
 from data import create_dataloader, create_dataset
 
+
 opt = {}
+
 # # subset and HR path only
 # opt['name'] = 'ImageNet'
 # opt['dataroot_HR'] = '/mnt/SSD/xtwang/ImageNet_train'
 # opt['dataroot_LR'] = None
-# opt['subset_file'] = '/mnt/SSD/xtwang/BasicSR/ImageNet_list.txt'
+# opt['subset_file'] = '/mnt/SSD/xtwang/BasicSR_datasets/ImageNet_list.txt'
 
-# # HR path only
-# opt['name'] = 'DIV2K800'
-# opt['dataroot_HR'] = '/mnt/SSD/xtwang/BasicSR/DIV2K800_sub'
-# opt['dataroot_LR'] = None
-# opt['subset_file'] = None
-
-# HR path and LR path
 opt['name'] = 'DIV2K'
 opt['dataroot_HR'] = '/mnt/SSD/xtwang/BasicSR_datasets/DIV2K800/DIV2K800_sub'
-opt['dataroot_LR'] = '/mnt/SSD/xtwang/BasicSR_datasets/DIV2K800/DIV2K800_sub_bicLR'
+opt['dataroot_LR'] = '/mnt/SSD/xtwang/BasicSR_datasets/DIV2K800/DIV2K800_sub_bicLRx4'
 opt['subset_file'] = None
-opt['dataroot_ref'] = '/mnt/SSD/xtwang/BasicSR_datasets/ISO/fusion_merge_bicLRx2_100'
-opt['reverse'] = True
+
+opt['dataroot_ref'] = None
+opt['reverse'] = False
+
 opt['data_type'] = 'img'
 opt['mode'] = 'LRHRref'
 opt['phase'] = 'train'
@@ -39,7 +36,9 @@ for i, data in enumerate(train_loader):
     print(i)
     LR = data['LR']
     HR = data['HR']
-    ref = data['ref']
+    if 'ref' in data:
+        ref = data['ref']
+        torchvision.utils.save_image(ref, 'ref_{:03d}.png'.format(i), nrow=8, padding=2, \
+            normalize=False)
     torchvision.utils.save_image(LR, 'LR_{:03d}.png'.format(i), nrow=8, padding=2, normalize=False)
     torchvision.utils.save_image(HR, 'HR_{:03d}.png'.format(i), nrow=8, padding=2, normalize=False)
-    torchvision.utils.save_image(ref, 'ref_{:03d}.png'.format(i), nrow=8, padding=2, normalize=False)
