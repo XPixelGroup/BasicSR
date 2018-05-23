04908dff0a5c177eff3e5be99ca182e8d9a5df80
XintaoWang <wxt1994@126.com>
2018-05-23 18:05:43 +0800

    add data/LRHR_dataset

--- codes/data/__init__.py
+++ codes/data/__init__.py
@@ -13,15 +13,15 @@ def create_dataloader(dataset, dataset_opt):
         num_workers = 1
     return torch.utils.data.DataLoader(
         dataset, batch_size=batch_size, shuffle=shuffle, num_workers=num_workers, pin_memory=True)
-    # Use paged-locked memory. Much Faster CPU <--> GPU data IO but consume more physical memory.
+    # Use paged-locked memory. Much faster CPU <--> GPU data IO but consume more physical memory.
 
 
 def create_dataset(dataset_opt):
     mode = dataset_opt['mode']
     if mode == 'LRHRref':
         from data.LRHRref_dataset import LRHRRefDataset as D
-    elif mode == 'NoiseHR':
-        from data.NoiseHR_dataset import NoiseHRDataset as D
+    if mode == 'LRHR':
+        from data.LRHR_dataset import LRHRDataset as D
     else:
         raise NotImplementedError("Dataset [%s] is not recognized." % mode)
     dataset = D(dataset_opt)
