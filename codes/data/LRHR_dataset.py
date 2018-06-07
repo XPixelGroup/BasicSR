7379866084852469fc466db4f36ce40ccaf94829
XintaoWang <wxt1994@126.com>
2018-06-07 23:43:46 +0800

    modify data

--- codes/data/LRHR_dataset.py
+++ codes/data/LRHR_dataset.py
@@ -23,16 +23,14 @@ class LRHRDataset(data.Dataset):
         self.paths_LR = []
         self.paths_HR = []
 
-        # read lmdb files
+        # read image list from lmdb or image files
         if opt['data_type'] == 'lmdb':
             if opt['dataroot_LR'] is not None:
                 self.LR_env, self.paths_LR = util.get_paths_from_lmdb(opt['dataroot_LR'])
             if opt['dataroot_HR'] is not None:
                 self.HR_env, self.paths_HR = util.get_paths_from_lmdb(opt['dataroot_HR'])
-        # read image files
         else:
-            if opt['phase'] == 'train' and opt['subset_file'] is not None:
-                # get HR image paths from list
+            if opt['phase'] == 'train' and opt['subset_file'] is not None:  # from subset list
                 with open(opt['subset_file']) as f:
                     self.paths_HR = sorted([os.path.join(opt['dataroot_HR'], line.rstrip('\n')) \
                             for line in f])
@@ -44,11 +42,13 @@ class LRHRDataset(data.Dataset):
                 if opt['dataroot_HR'] is not None:
                     self.paths_HR = sorted(util.get_image_paths(opt['dataroot_HR']))
 
-        assert self.paths_HR, 'Error: HR paths are empty.'  # must have HR paths.
+        assert self.paths_HR, 'Error: HR paths are empty.'
         if self.paths_LR and self.paths_HR:
             assert len(self.paths_LR) == len(self.paths_HR), \
                 'HR and LR datasets have different number of images - {}, {}.'.format(\
                 len(self.paths_LR), len(self.paths_HR))
+        # randomly scale list
+        self.random_scale_list = [1, 0.9, 0.8, 0.7, 0.6, 0.5]
 
     def __getitem__(self, index):
         HR_path, LR_path = None, None
@@ -58,10 +58,10 @@ class LRHRDataset(data.Dataset):
         HR_path = self.paths_HR[index]
         if self.opt['data_type'] == 'img':
             img_HR = cv2.imread(HR_path, cv2.IMREAD_UNCHANGED)
-        else:  # lmdb
+        else:
             img_HR = util.read_lmdb_img(self.HR_env, HR_path)
-        img_HR = img_HR * 1.0 / 255  # numpy.ndarray(float64), [0,1], HWC, BGR
-        if img_HR.ndim == 2:  # gray image
+        img_HR = img_HR.astype(np.float32) / 255.
+        if img_HR.ndim == 2:
             img_HR = np.expand_dims(img_HR, axis=2)
 
         # get LR image
@@ -69,20 +69,31 @@ class LRHRDataset(data.Dataset):
             LR_path = self.paths_LR[index]
             if self.opt['data_type'] == 'img':
                 img_LR = cv2.imread(LR_path, cv2.IMREAD_UNCHANGED)
-            else:  # lmdb
+            else:
                 img_LR = util.read_lmdb_img(self.LR_env, LR_path)
-            img_LR = img_LR * 1.0 / 255
-        # down-sampling on-the-fly
-        else:
+            img_LR = img_LR.astype(np.float32) / 255.
+        else:  # down-sampling on-the-fly
+            # randomly scale during training
+            if self.opt['phase'] == 'train':
+                HR_size = self.opt['HR_size']
+                random_scale = random.choice(self.random_scale_list)
+                H_s, W_s, _ = img_HR.shape
+                H_s, W_s = int(H_s * random_scale), int(W_s * random_scale)
+                if H_s < HR_size:
+                    H_s = HR_size
+                if W_s < HR_size:
+                    W_s = HR_size
+                img_HR = cv2.resize(img_HR, (W_s, H_s), interpolation=cv2.INTER_LINEAR)
+
             H, W, _ = img_HR.shape
             # using INTER_LINEAR now
+            # TODO support matlab resize
             img_LR = cv2.resize(img_HR, (W // scale, H // scale), interpolation=cv2.INTER_LINEAR)
         if img_LR.ndim == 2:
             img_LR = np.expand_dims(img_LR, axis=2)
 
         H, W, C = img_LR.shape
         if self.opt['phase'] == 'train':
-            HR_size = self.opt['HR_size']
             LR_size = HR_size // scale
 
             # randomly crop
@@ -92,50 +103,22 @@ class LRHRDataset(data.Dataset):
             rnd_h_HR, rnd_w_HR = int(rnd_h * scale), int(rnd_w * scale)
             img_HR = img_HR[rnd_h_HR:rnd_h_HR + HR_size, rnd_w_HR:rnd_w_HR + HR_size, :]
 
-            # augmentation - flip, rotation
-            hflip = self.opt['use_flip'] and random.random() > 0.5
-            vflip = self.opt['use_rot'] and random.random() > 0.5
-            rot90 = self.opt['use_rot'] and random.random() > 0.5
-
-            def _augment(img):
-                if hflip: img = img[:, ::-1, :]
-                if vflip: img = img[::-1, :, :]
-                if rot90: img = img.transpose(1, 0, 2)
-                return img
-
-            img_HR = _augment(img_HR)
-            img_LR = _augment(img_LR)
+            # augmentation - flip, rotate
+            img_LR, img_HR = util.augment([img_LR, img_HR], self.opt['use_flip'], self.opt['use_rot'])
 
         # channel conversion
-        if C == 3 and self.opt['color'] == 'gray':  # RGB to gray
-            img_HR = np.dot(img_HR[..., :3], [0.2989, 0.587, 0.114])
-            img_LR = np.dot(img_LR[..., :3], [0.2989, 0.587, 0.114])
-            img_HR = np.expand_dims(img_HR, axis=2)
-            img_LR = np.expand_dims(img_LR, axis=2)
-        elif C == 3 and self.opt['color'] == 'y':  # RGB to y
-            img_HR = np.dot(img_HR[..., :3],
-                            [65.481 / 255, 128.553 / 255, 24.966 / 255]) + 16.0 / 255
-            img_LR = np.dot(img_LR[..., :3],
-                            [65.481 / 255, 128.553 / 255, 24.966 / 255]) + 16.0 / 255
-            img_HR = np.expand_dims(img_HR, axis=2)
-            img_LR = np.expand_dims(img_LR, axis=2)
-        elif C == 1 and self.opt['color'] == 'RGB':  # gray/y to RGB
-            img_HR = np.repeat(img_HR, 3, axis=2)
-            img_LR = np.repeat(img_LR, 3, axis=2)
+        if self.opt['color']:
+            img_LR, img_HR = util.channel_convert(C, self.opt['color'], [img_LR, img_HR])
 
-        # numpy to tensor, HWC to CHW, BGR to RGB
+        # HWC to CHW, BGR to RGB, numpy to tensor
         if img_HR.shape[2] == 3:
-            img_HR = torch.from_numpy(np.transpose(img_HR[:, :, [2, 1, 0]], (2, 0, 1))).float()
-            img_LR = torch.from_numpy(np.transpose(img_LR[:, :, [2, 1, 0]], (2, 0, 1))).float()
-        else:
-            img_HR = torch.from_numpy(np.ascontiguousarray(np.transpose(img_HR, (2, 0, 1)))).float()
-            img_LR = torch.from_numpy(np.ascontiguousarray(np.transpose(img_LR, (2, 0, 1)))).float()
+            img_HR = cv2.cvtColor(img_HR, cv2.COLOR_BGR2RGB)
+            img_LR = cv2.cvtColor(img_LR, cv2.COLOR_BGR2RGB)
+        img_HR = torch.from_numpy(np.ascontiguousarray(np.transpose(img_HR, (2, 0, 1)))).float()
+        img_LR = torch.from_numpy(np.ascontiguousarray(np.transpose(img_LR, (2, 0, 1)))).float()
 
         if LR_path is None:
-            LR_path = HR_path
-        if self.opt['reverse']:
-            img_LR, img_HR = img_HR, img_LR
-            LR_path, HR_path = HR_path, LR_path
+            LR_path = 'on-the-fly'
         return {'LR': img_LR, 'HR': img_HR, 'LR_path': LR_path, 'HR_path': HR_path}
 
     def __len__(self):
