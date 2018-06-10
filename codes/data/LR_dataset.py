7c9649a683d5c48228fcdc1129f4cd8e070b40b5
XintaoWang <wxt1994@126.com>
2018-06-11 00:53:20 +0800

    update data/
    Former-commit-id: a248b51b3c644a66ee7494ca22c10c83519d83f7
    Former-commit-id: 2da716773109a144240c43499b9d537d0006d6b5

--- codes/data/LR_dataset.py
+++ codes/data/LR_dataset.py
@@ -8,7 +8,7 @@ import data.util as util
 
 class LRDataset(data.Dataset):
     '''
-    Read LR images only for testing.
+    Read LR images only in test phase.
     '''
 
     def name(self):
@@ -17,13 +17,11 @@ class LRDataset(data.Dataset):
     def __init__(self, opt):
         super(LRDataset, self).__init__()
         self.opt = opt
-        self.paths_LR = []
+        self.paths_LR = None
+        self.LR_env = None  # environment for lmdb
 
         # read image list from lmdb or image files
-        if opt['data_type'] == 'lmdb':
-            self.LR_env, self.paths_LR = util.get_paths_from_lmdb(opt['dataroot_LR'])
-        else:
-            self.paths_LR = sorted(util.get_image_paths(opt['dataroot_LR']))
+        self.LR_env, self.paths_LR = util.get_image_paths(opt['data_type'], opt['dataroot_LR'])
         assert self.paths_LR, 'Error: LR paths are empty.'
 
     def __getitem__(self, index):
@@ -31,13 +29,7 @@ class LRDataset(data.Dataset):
 
         # get LR image
         LR_path = self.paths_LR[index]
-        if self.opt['data_type'] == 'img':
-            img_LR = cv2.imread(LR_path, cv2.IMREAD_UNCHANGED)
-        else:
-            img_LR = util.read_lmdb_img(self.LR_env, LR_path)
-        img_LR = img_LR.astype(np.float32) / 255.
-        if img_LR.ndim == 2:
-            img_LR = np.expand_dims(img_LR, axis=2)
+        img_LR = util.read_img(self.LR_env, LR_path)
         H, W, C = img_LR.shape
 
         # channel conversion
@@ -46,7 +38,7 @@ class LRDataset(data.Dataset):
 
         # HWC to CHW, BGR to RGB, numpy to tensor
         if img_LR.shape[2] == 3:
-            img_LR = cv2.cvtColor(img_LR, cv2.COLOR_BGR2RGB)
+            img_LR = img_LR[:, :, [2, 1, 0]]
         img_LR = torch.from_numpy(np.ascontiguousarray(np.transpose(img_LR, (2, 0, 1)))).float()
 
         return {'LR': img_LR, 'LR_path': LR_path}
