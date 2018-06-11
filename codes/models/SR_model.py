a029d65fa3025e0a7846f81b038b031b372edf92
XintaoWang <wxt1994@126.com>
2018-06-11 15:18:25 +0800

    big modification
    Former-commit-id: 183bb03bb3d6e1babea64c4a4038fd6307faa017
    Former-commit-id: ec0e07033bf0f929d03b7a7311c9f4e1d42ffcc7

--- codes/models/SR_model.py
+++ codes/models/SR_model.py
@@ -27,40 +27,40 @@ class SRModel(BaseModel):
         if self.is_train:
             self.netG.train()
 
-            # define loss function
+            # loss
             loss_type = train_opt['pixel_criterion']
             if loss_type == 'l1':
-                self.criterion_pixel = nn.L1Loss()
+                self.cri_pix = nn.L1Loss()
             elif loss_type == 'l2':
-                self.criterion_pixel = nn.MSELoss()
+                self.cri_pix = nn.MSELoss()
             else:
                 raise NotImplementedError('Loss type [%s] is not recognized.' % loss_type)
             if self.use_gpu:
-                self.criterion_pixel.cuda()
-            self.loss_pixel_weight = train_opt['pixel_weight']
+                self.cri_pix.cuda()
+            self.l_pix_w = train_opt['pixel_weight']
 
-            # initialize optimizers
+            # optimizers
             self.optimizers = []
-            self.lr_G = train_opt['lr_G']
-            self.wd_G = train_opt['weight_decay_G'] if train_opt['weight_decay_G'] else 0
+            wd_G = train_opt['weight_decay_G'] if train_opt['weight_decay_G'] else 0
             optim_params = []
-            # can optimize for a part of the model
-            for k, v in self.netG.named_parameters():
+            for k, v in self.netG.named_parameters():  # can optimize for a part of the model
                 if v.requires_grad:
                     optim_params.append(v)
                 else:
                     print('WARNING: params [%s] will not optimize.' % k)
-            self.optimizer_G = torch.optim.Adam(optim_params, lr=self.lr_G, weight_decay=self.wd_G)
+            self.optimizer_G = torch.optim.Adam(optim_params, lr=train_opt['lr_G'], weight_decay=wd_G)
             self.optimizers.append(self.optimizer_G)
 
-            # initialize schedulers
+            # schedulers
             self.schedulers = []
             if train_opt['lr_scheme'] == 'MultiStepLR':
-                self.scheduler_G = lr_scheduler.MultiStepLR(self.optimizer_G, \
-                    train_opt['lr_steps'], train_opt['lr_gamma'])
+                for optimizer in self.optimizers:
+                    self.schedulers.append(lr_scheduler.MultiStepLR(optimizer, \
+                        train_opt['lr_steps'], train_opt['lr_gamma']))
             else:
                 raise NotImplementedError('MultiStepLR learning rate scheme is enough.')
-            self.schedulers.append(self.scheduler_G)
+
+            self.log_dict = OrderedDict()
 
         print('---------- Model initialized ------------------')
         self.print_network()
@@ -70,53 +70,41 @@ class SRModel(BaseModel):
         # LR
         input_L = data['LR']
         self.input_L.resize_(input_L.size()).copy_(input_L)
-        self.real_L = Variable(self.input_L, volatile=volatile)
+        self.var_L = Variable(self.input_L, volatile=volatile)
 
         if need_HR:
             input_H = data['HR']
             self.input_H.resize_(input_H.size()).copy_(input_H)
             self.real_H = Variable(self.input_H, volatile=volatile)
 
-        # import torchvision.utils
-        # torchvision.utils.save_image(input_L, 'LR.png', nrow=4, padding=2, normalize=False)
-        # torchvision.utils.save_image(input_H, 'HR.png', nrow=4, padding=2, normalize=False)
-
-    def forward_G(self):
-        self.fake_H = self.netG(self.real_L)
-
-    def backward_G(self):
-        self.loss_pixel = self.loss_pixel_weight * self.criterion_pixel(self.fake_H, self.real_H)
-        self.loss_pixel.backward()
-
     def optimize_parameters(self, step):
-        self.forward_G()
         self.optimizer_G.zero_grad()
-        self.backward_G()
+        self.fake_H = self.netG(self.var_L)
+        l_pix = self.l_pix_w * self.cri_pix(self.fake_H, self.real_H)
+        l_pix.backward()
         self.optimizer_G.step()
+
+        # set log
+        self.log_dict['l_pix'] = l_pix.data[0]
 
     def test(self):
         self.netG.eval()
-        self.fake_H = self.netG(self.real_L)
+        self.fake_H = self.netG(self.var_L)
         self.netG.train()
 
-    def get_current_losses(self):
-        out_dict = OrderedDict()
-        out_dict['loss_pixel'] = self.loss_pixel.data[0]
-        return out_dict
-
-    def get_more_training_info(self):
-        return None
+    def get_current_log(self):
+        return self.log_dict
 
     def get_current_visuals(self, need_HR=True):
         out_dict = OrderedDict()
-        out_dict['LR'] = self.real_L.data[0].float().cpu()
+        out_dict['LR'] = self.var_L.data[0].float().cpu()
         out_dict['SR'] = self.fake_H.data[0].float().cpu()
         if need_HR:
             out_dict['HR'] = self.real_H.data[0].float().cpu()
         return out_dict
 
     def print_network(self):
-        s, n = self.get_network_decsription(self.netG)
+        s, n = self.get_network_description(self.netG)
         print('Number of parameters in G: {:,d}'.format(n))
         if self.is_train:
             message = '-------------- Generator --------------\n' + s + '\n'
--- codes/models/__init__.py
+++ codes/models/__init__.py
@@ -6,7 +6,7 @@ def create_model(opt):
     elif model == 'srgan':
         from .SRGAN_model import SRGANModel as M
     elif model == 'sftgan':
-        from .SFTGAN_model import SFTModel as M
+        from .SFTGAN_ACD_model import SFTGAN_ACD_Model as M
     else:
         raise NotImplementedError('Model [%s] not recognized.' % model)
     m = M(opt)
