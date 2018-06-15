100a16f0dbab11e40c21cfe67d3b019398a26505
XintaoWang <wxt1994@126.com>
2018-06-15 13:13:08 +0800

    update model
    Former-commit-id: 9de5104fefa8661613642f5bfd3c89a4470e37ca

--- codes/models/modules/sft_arch.py
+++ codes/models/modules/sft_arch.py
@@ -86,70 +86,6 @@ class SFT_Net(nn.Module):
 
 
 # Auxiliary Classifier Discriminator
-class ACD_VGG_BN_128(nn.Module):
-    def __init__(self):
-        super(ACD_VGG_BN_128, self).__init__()
-
-        self.feature = nn.Sequential(
-            nn.Conv2d(3, 64, 3, 1, 1),
-            nn.LeakyReLU(0.1, True),
-
-            nn.Conv2d(64, 64, 4, 2, 1),
-            nn.BatchNorm2d(64, affine=True),
-            nn.LeakyReLU(0.1, True),
-
-            nn.Conv2d(64, 128, 3, 1, 1),
-            nn.BatchNorm2d(128, affine=True),
-            nn.LeakyReLU(0.1, True),
-
-            nn.Conv2d(128, 128, 4, 2, 1),
-            nn.BatchNorm2d(128, affine=True),
-            nn.LeakyReLU(0.1, True),
-
-            nn.Conv2d(128, 256, 3, 1, 1),
-            nn.BatchNorm2d(256, affine=True),
-            nn.LeakyReLU(0.1, True),
-
-            nn.Conv2d(256, 256, 4, 2, 1),
-            nn.BatchNorm2d(256, affine=True),
-            nn.LeakyReLU(0.1, True),
-
-            nn.Conv2d(256, 512, 3, 1, 1),
-            nn.BatchNorm2d(512, affine=True),
-            nn.LeakyReLU(0.1, True),
-
-            nn.Conv2d(512, 512, 4, 2, 1),
-            nn.BatchNorm2d(512, affine=True),
-            nn.LeakyReLU(0.1, True),
-
-            nn.Conv2d(512, 512, 3, 1, 1),
-            nn.BatchNorm2d(512, affine=True),
-            nn.LeakyReLU(0.1, True),
-
-            nn.Conv2d(512, 512, 4, 2, 1)
-        )
-
-        # gan
-        self.gan = nn.Sequential(
-            nn.Linear(512*4*4, 100),
-            nn.LeakyReLU(0.1, True),
-            nn.Linear(100, 1)
-        )
-
-        self.cls = nn.Sequential(
-            nn.Linear(512*4*4, 100),
-            nn.LeakyReLU(0.1, True),
-            nn.Linear(100, 7)
-        )
-
-    def forward(self, x):
-        fea = self.feature(x)
-        fea = fea.view(fea.size(0), -1)
-        gan = self.gan(fea)
-        cls = self.cls(fea)
-        return [gan, cls]
-
-
 class ACD_VGG_BN_96(nn.Module):
     def __init__(self):
         super(ACD_VGG_BN_96, self).__init__()
@@ -195,62 +131,9 @@ class ACD_VGG_BN_96(nn.Module):
         )
 
         self.cls = nn.Sequential(
-            nn.Linear(512*6*6, 7),
-        )
-
-    def forward(self, x):
-        fea = self.feature(x)
-        fea = fea.view(fea.size(0), -1)
-        gan = self.gan(fea)
-        cls = self.cls(fea)
-        return [gan, cls]
-
-class ACD_VGG_BN_128_SN(nn.Module):  # with spectral_norm
-    def __init__(self):
-        super(ACD_VGG_BN_128_SN, self).__init__()
-
-        self.feature = nn.Sequential(
-            spectral_norm(nn.Conv2d(3, 64, 3, 1, 1)),
+            nn.Linear(512*6*6, 100),
             nn.LeakyReLU(0.1, True),
-
-            spectral_norm(nn.Conv2d(64, 64, 4, 2, 1)),
-            nn.LeakyReLU(0.1, True),
-
-            spectral_norm(nn.Conv2d(64, 128, 3, 1, 1)),
-            nn.LeakyReLU(0.1, True),
-
-            spectral_norm(nn.Conv2d(128, 128, 4, 2, 1)),
-            nn.LeakyReLU(0.1, True),
-
-            spectral_norm(nn.Conv2d(128, 256, 3, 1, 1)),
-            nn.LeakyReLU(0.1, True),
-
-            spectral_norm(nn.Conv2d(256, 256, 4, 2, 1)),
-            nn.LeakyReLU(0.1, True),
-
-            spectral_norm(nn.Conv2d(256, 512, 3, 1, 1)),
-            nn.LeakyReLU(0.1, True),
-
-            spectral_norm(nn.Conv2d(512, 512, 4, 2, 1)),
-            nn.LeakyReLU(0.1, True),
-
-            spectral_norm(nn.Conv2d(512, 512, 3, 1, 1)),
-            nn.LeakyReLU(0.1, True),
-
-            spectral_norm(nn.Conv2d(512, 512, 4, 2, 1))
-        )
-
-        # gan
-        self.gan = nn.Sequential(
-            spectral_norm(nn.Linear(512*4*4, 100)),
-            nn.LeakyReLU(0.1, True),
-            spectral_norm(nn.Linear(100, 1))
-        )
-
-        self.cls = nn.Sequential(
-            spectral_norm(nn.Linear(512*4*4, 100)),
-            nn.LeakyReLU(0.1, True),
-            spectral_norm(nn.Linear(100, 7))
+            nn.Linear(100, 8)
         )
 
     def forward(self, x):
@@ -261,39 +144,3 @@ class ACD_VGG_BN_128_SN(nn.Module):  # with spectral_norm
         return [gan, cls]
 
 
-if __name__ == '__main__':
-    import os.path
-    import cv2
-    import numpy as np
-    from torch.autograd import Variable
-    import sys
-    sys.path.insert(0, os.path.abspath('../../'))
-    from data.util import imresize
-    import torchvision.utils
-
-    # sft network
-    sft_net = SFT_Net()
-    # # save model
-    # save_path = '/home/xtwang/Projects/BasicSR/codes/scripts/sft_net_raw.pth'
-    # state_dict = sft_net.state_dict()
-    # torch.save(state_dict, save_path)
-
-    load_path = '/home/xtwang/Projects/BasicSR/codes/scripts/sft_net.pth'
-    sft_net.load_state_dict(torch.load(load_path), strict=True)
-    sft_net.eval()
-    sft_net.cuda()
-
-    img = cv2.imread('/mnt/SSD/xtwang/BasicSR_datasets/OST/test/img/OST300/OST_013.png', cv2.IMREAD_UNCHANGED)
-    img = img * 1.0 / 255
-    img = torch.from_numpy(np.transpose(img[:, :, [2, 1, 0]], (2, 0, 1))).float()
-    # matlab imresize
-    img_LR = imresize(img, 1 / 4, antialiasing=True)
-    img_LR = img_LR.unsqueeze(0)
-    img_LR = img_LR.cuda()
-
-    seg = torch.load('/mnt/SSD/xtwang/BasicSR_datasets/OST/test/bicseg/OST300/OST_013.pth')
-    seg = seg.unsqueeze(0).cuda()
-
-    output = sft_net((Variable(img_LR, volatile=True), Variable(seg))).data.float().cpu()
-    output.squeeze_()
-    torchvision.utils.save_image(output, 'rlt.png', padding=0, normalize=False)
