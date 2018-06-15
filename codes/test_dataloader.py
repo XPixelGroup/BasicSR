5b5a075bb8f467c634302b4f3a62c775fe3fe9cf
XintaoWang <wxt1994@126.com>
2018-06-15 20:51:12 +0800

    modify test_dataloader

--- codes/test_dataloader.py
+++ codes/test_dataloader.py
@@ -1,5 +1,7 @@
 import time
 import math
+import torch
+import numpy as np
 import torchvision.utils
 from data import create_dataloader, create_dataset
 from utils import util
@@ -9,19 +11,59 @@ opt = {}
 opt['name'] = 'test'
 opt['dataroot_HR'] = '/mnt/SSD/xtwang/BasicSR_datasets/DIV2K800/DIV2K800_sub.lmdb'
 opt['dataroot_LR'] = '/mnt/SSD/xtwang/BasicSR_datasets/DIV2K800/DIV2K800_sub_bicLRx4.lmdb'
+
+# opt['dataroot_HR'] = '/mnt/SSD/xtwang/BasicSR_datasets/OST/train/img'
+# opt['dataroot_HR_bg'] = '/mnt/SSD/xtwang/BasicSR_datasets/DIV2K800/DIV2K800_sub'
+# opt['dataroot_LR'] = None
+
 opt['subset_file'] = None
-opt['mode'] = 'LRHR'
-opt['phase'] = 'train'
+opt['mode'] = 'LRHR'  # 'LRHR' | 'LRHRseg_bg'
+opt['phase'] = 'train' # 'train' | 'val'
 opt['use_shuffle'] = True
 opt['n_workers'] = 8
 opt['batch_size'] = 16
-opt['HR_size'] = 192
+opt['HR_size'] = 96
 opt['scale'] = 4
 opt['use_flip'] = True
 opt['use_rot'] = True
 opt['color'] = 'RGB'
 
-opt['data_type'] = 'lmdb'
+opt['data_type'] = 'lmdb' # img lmdb
+
+# for segmentation
+# look_up table # RGB
+lookup_table = torch.from_numpy(
+    np.array([
+        [153, 153, 153],  # 0, background
+        [0, 255, 255],  # 1, sky
+        [109, 158, 235],  # 2, water
+        [183, 225, 205],  # 3, grass
+        [153, 0, 255],  # 4, mountain
+        [17, 85, 204],  # 5, building
+        [106, 168, 79],  # 6, plant
+        [224, 102, 102],  # 7, animal
+        [255, 255, 255],  # 8/255, void
+    ])).float()
+lookup_table /= 255
+
+def render(seg):
+    _, argmax = torch.max(seg, 0)
+    argmax = argmax.squeeze().byte()
+    # color img
+    im_h, im_w = argmax.size()
+    color = torch.FloatTensor(3, im_h, im_w).fill_(0)  # black
+    for k in range(8):
+        mask = torch.eq(argmax, k)
+        color.select(0, 0).masked_fill_(mask, lookup_table[k][0])  # R
+        color.select(0, 1).masked_fill_(mask, lookup_table[k][1])  # G
+        color.select(0, 2).masked_fill_(mask, lookup_table[k][2])  # B
+    # void
+    mask = torch.eq(argmax, 255)
+    color.select(0, 0).masked_fill_(mask, lookup_table[8][0])  # R
+    color.select(0, 1).masked_fill_(mask, lookup_table[8][1])  # G
+    color.select(0, 2).masked_fill_(mask, lookup_table[8][2])  # B
+    return color
+
 
 util.mkdir('tmp')
 train_set = create_dataset(opt)
@@ -48,3 +90,14 @@ for i, data in enumerate(train_loader):
         LR, 'tmp/LR_{:03d}.png'.format(i), nrow=nrow, padding=padding, normalize=False)
     torchvision.utils.save_image(
         HR, 'tmp/HR_{:03d}.png'.format(i), nrow=nrow, padding=padding, normalize=False)
+
+    if opt['mode'] == 'LRHRseg_bg':
+        seg = data['seg']
+        seg_color_list = []
+        for j in range(seg.size(0)):
+            _seg = seg[j, :, :, :]
+            seg_color_list.append(render(_seg).unsqueeze_(0))
+
+        seg_color_batch = torch.cat(seg_color_list, 0)
+        torchvision.utils.save_image(
+            seg_color_batch, 'tmp/seg_{:03d}.png'.format(i), nrow=nrow, padding=2, normalize=False)
