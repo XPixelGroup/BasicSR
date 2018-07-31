cd464f54184ae09a4ac1d504d921341d3ce1b7a2
XintaoWang <wxt1994@126.com>
2018-07-31 21:22:49 +0800

    modify model/__init
    Former-commit-id: 9d0c351d4d891834e6ea14b5aed5d289cc404822
    Former-commit-id: d515721d98358076193946517124b9b61e22192d

--- codes/models/__init__.py
+++ codes/models/__init__.py
@@ -5,7 +5,7 @@ def create_model(opt):
         from .SR_model import SRModel as M
     elif model == 'srgan':
         from .SRGAN_model import SRGANModel as M
-    elif model == 'srsagan':
+    elif model == 'srragan':
         from .SRRaGAN_model import SRRaGANModel as M
     elif model == 'sftgan':
         from .SFTGAN_ACD_model import SFTGAN_ACD_Model as M
