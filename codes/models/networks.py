d74dedea068dc856af8408cd6edc3314f17d99ce
XintaoWang <wxt1994@126.com>
2018-08-30 22:55:01 +0800

    networks.py init modify function name

--- codes/models/networks.py
+++ codes/models/networks.py
@@ -14,49 +14,49 @@ def weights_init_normal(m, std=0.02):
     classname = m.__class__.__name__
     # print('initializing [%s] ...' % classname)
     if classname.find('Conv') != -1:
-        init.normal(m.weight.data, 0.0, std)
+        init.normal_(m.weight.data, 0.0, std)
         if m.bias is not None:
             m.bias.data.zero_()
     elif classname.find('Linear') != -1:
-        init.normal(m.weight.data, 0.0, std)
+        init.normal_(m.weight.data, 0.0, std)
         if m.bias is not None:
             m.bias.data.zero_()
     elif classname.find('BatchNorm2d') != -1:
-        init.normal(m.weight.data, 1.0, std)
-        init.constant(m.bias.data, 0.0)
+        init.normal_(m.weight.data, 1.0, std)
+        init.constant_(m.bias.data, 0.0)
 
 def weights_init_kaiming(m, scale=1):
     classname = m.__class__.__name__
     if classname.find('Conv') != -1:
         # print('initializing [%s] ...' % classname)
-        init.kaiming_normal(m.weight.data, a=0, mode='fan_in')
+        init.kaiming_normal_(m.weight.data, a=0, mode='fan_in')
         m.weight.data *= scale
         if m.bias is not None:
             m.bias.data.zero_()
     elif classname.find('Linear') != -1:
-        init.kaiming_normal(m.weight.data, a=0, mode='fan_in')
+        init.kaiming_normal_(m.weight.data, a=0, mode='fan_in')
         m.weight.data *= scale
         if m.bias is not None:
             m.bias.data.zero_()
     elif classname.find('BatchNorm2d') != -1:
-        init.constant(m.weight.data, 1.0)
+        init.constant_(m.weight.data, 1.0)
         m.weight.data *= scale
-        init.constant(m.bias.data, 0.0)
+        init.constant_(m.bias.data, 0.0)
 
 def weights_init_orthogonal(m):
     classname = m.__class__.__name__
     # print('initializing [%s] ...' % classname)
     if classname.find('Conv') != -1:
-        init.orthogonal(m.weight.data, gain=1)
+        init.orthogonal_(m.weight.data, gain=1)
         if m.bias is not None:
             m.bias.data.zero_()
     elif classname.find('Linear') != -1:
-        init.orthogonal(m.weight.data, gain=1)
+        init.orthogonal_(m.weight.data, gain=1)
         if m.bias is not None:
             m.bias.data.zero_()
     elif classname.find('BatchNorm2d') != -1:
-        init.normal(m.weight.data, 1.0, 0.02)
-        init.constant(m.bias.data, 0.0)
+        init.normal_(m.weight.data, 1.0, 0.02)
+        init.constant_(m.bias.data, 0.0)
 
 def init_weights(net, init_type='kaiming', scale=1, std=0.02):
     # scale for 'kaiming', std for 'normal'.
