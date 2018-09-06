d428f20e309225c84d3da903505374c225c25e29
XintaoWang <wxt1994@126.com>
2018-09-06 20:27:10 +0800

    create_lmdb.py add progress bar
    Former-commit-id: 23a45d5eb839c763c94c0e413ad599d9f9b5e184

--- codes/scripts/create_lmdb.py
+++ codes/scripts/create_lmdb.py
@@ -1,52 +1,55 @@
+import sys
 import os.path
 import glob
 import pickle
 import lmdb
 import cv2
 
-# configurations
-img_path = '/mnt/SSD/xtwang/BasicSR_datasets/DIV2K800/DIV2K800_sub/*'  # glob matching pattern
-lmdb_save_path = '/mnt/SSD/xtwang/BasicSR_datasets/DIV2K800/DIV2K800_sub.lmdb'  # must end with .lmdb
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+from utils.progress_bar import ProgressBar
 
-file_list = sorted(glob.glob(img_path))
+# configurations
+img_folder = '/mnt/SSD/xtwang/BasicSR_datasets/DIV2K800/DIV2K800/*'  # glob matching pattern
+lmdb_save_path = '/mnt/SSD/xtwang/BasicSR_datasets/DIV2K800/DIV2K800.lmdb'  # must end with .lmdb
+
+img_list = sorted(glob.glob(img_folder))
 dataset = []
 data_size = 0
 
-print('Read image...')
-for i, v in enumerate(file_list):
-    if i % 100 == 0:
-        print(i)
+print('Read images...')
+pbar = ProgressBar(len(img_list))
+for i, v in enumerate(img_list):
+    pbar.update('Read {}'.format(v))
     img = cv2.imread(v, cv2.IMREAD_UNCHANGED)
     dataset.append(img)
     data_size += img.nbytes
-map_size = data_size * 10
-env = lmdb.open(lmdb_save_path, map_size=map_size)
+env = lmdb.open(lmdb_save_path, map_size=data_size * 10)
+print('Finish reading {} images.\nWrite lmdb...'.format(len(img_list)))
 
-print('Finish reading image - {}.\nWrite to lmdb...'.format(len(file_list)))
+pbar = ProgressBar(len(img_list))
 with env.begin(write=True) as txn:  # txn is a Transaction object
-    for i, v in enumerate(file_list):
-        if i % 100 == 0:
-            print(i)
-        img_name = os.path.splitext(os.path.basename(v))[0]
-        key = img_name.encode('ascii')
+    for i, v in enumerate(img_list):
+        pbar.update('Write {}'.format(v))
+        base_name = os.path.splitext(os.path.basename(v))[0]
+        key = base_name.encode('ascii')
         data = dataset[i]
         if dataset[i].ndim == 2:
             H, W = dataset[i].shape
             C = 1
         else:
             H, W, C = dataset[i].shape
-        meta_key = (img_name + '.meta').encode('ascii')
+        meta_key = (base_name + '.meta').encode('ascii')
         meta = '{:d}, {:d}, {:d}'.format(H, W, C)
         # The encode is only essential in Python 3
         txn.put(key, data)
         txn.put(meta_key, meta.encode('ascii'))
-print('Finish writing to lmdb - {}.'.format(len(file_list)))
+print('Finish writing lmdb.')
 
 # create keys cache
 keys_cache_file = os.path.join(lmdb_save_path, '_keys_cache.p')
 env = lmdb.open(lmdb_save_path, readonly=True, lock=False, readahead=False, meminit=False)
 with env.begin(write=False) as txn:
-    print('creating lmdb keys cache: {}'.format(keys_cache_file))
+    print('Create lmdb keys cache: {}'.format(keys_cache_file))
     keys = [key.decode('ascii') for key, _ in txn.cursor()]
     pickle.dump(keys, open(keys_cache_file, "wb"))
-print('Finish creating lmdb keys.')
+print('Finish creating lmdb keys cache.')
