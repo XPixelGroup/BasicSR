aafa7d641b25d7d983bcd1ce96fb4bd4f7616d21
XintaoWang <wxt1994@126.com>
2019-05-28 22:39:53 +0800

    update util

--- codes/utils/util.py
+++ codes/utils/util.py
@@ -1,12 +1,38 @@
 import os
+import sys
+import time
 import math
 from datetime import datetime
+import random
+import logging
+from collections import OrderedDict
 import numpy as np
 import cv2
-from torchvision.utils import make_grid
-import random
 import torch
-import logging
+from torchvision.utils import make_grid
+from shutil import get_terminal_size
+
+import yaml
+try:
+    from yaml import CLoader as Loader, CDumper as Dumper
+except ImportError:
+    from yaml import Loader, Dumper
+
+
+def OrderedYaml():
+    '''yaml orderedDict support'''
+    _mapping_tag = yaml.resolver.BaseResolver.DEFAULT_MAPPING_TAG
+
+    def dict_representer(dumper, data):
+        return dumper.represent_dict(data.items())
+
+    def dict_constructor(loader, node):
+        return OrderedDict(loader.construct_pairs(node))
+
+    Dumper.add_representer(OrderedDict, dict_representer)
+    Loader.add_constructor(_mapping_tag, dict_constructor)
+    return Loader, Dumper
+
 
 ####################
 # miscellaneous
@@ -47,16 +73,17 @@ def set_random_seed(seed):
     torch.cuda.manual_seed_all(seed)
 
 
-def setup_logger(logger_name, root, phase, level=logging.INFO, screen=False):
+def setup_logger(logger_name, root, phase, level=logging.INFO, screen=False, tofile=False):
     '''set up logger'''
     l = logging.getLogger(logger_name)
     formatter = logging.Formatter(
         '%(asctime)s.%(msecs)03d - %(levelname)s: %(message)s', datefmt='%y-%m-%d %H:%M:%S')
-    log_file = os.path.join(root, phase + '_{}.log'.format(get_timestamp()))
-    fh = logging.FileHandler(log_file, mode='w')
-    fh.setFormatter(formatter)
     l.setLevel(level)
-    l.addHandler(fh)
+    if tofile:
+        log_file = os.path.join(root, phase + '_{}.log'.format(get_timestamp()))
+        fh = logging.FileHandler(log_file, mode='w')
+        fh.setFormatter(formatter)
+        l.addHandler(fh)
     if screen:
         sh = logging.StreamHandler()
         sh.setFormatter(formatter)
@@ -132,8 +159,8 @@ def ssim(img1, img2):
     sigma2_sq = cv2.filter2D(img2**2, -1, window)[5:-5, 5:-5] - mu2_sq
     sigma12 = cv2.filter2D(img1 * img2, -1, window)[5:-5, 5:-5] - mu1_mu2
 
-    ssim_map = ((2 * mu1_mu2 + C1) * (2 * sigma12 + C2)) / ((mu1_sq + mu2_sq + C1) *
-                                                            (sigma1_sq + sigma2_sq + C2))
+    ssim_map = ((2 * mu1_mu2 + C1) * (2 * sigma12 + C2)) / (
+        (mu1_sq + mu2_sq + C1) * (sigma1_sq + sigma2_sq + C2))
     return ssim_map.mean()
 
 
@@ -156,3 +183,53 @@ def calculate_ssim(img1, img2):
             return ssim(np.squeeze(img1), np.squeeze(img2))
     else:
         raise ValueError('Wrong input image dimensions.')
+
+
+class ProgressBar(object):
+    '''A progress bar which can print the progress
+    modified from https://github.com/hellock/cvbase/blob/master/cvbase/progress.py
+    '''
+
+    def __init__(self, task_num=0, bar_width=50, start=True):
+        self.task_num = task_num
+        max_bar_width = self._get_max_bar_width()
+        self.bar_width = (bar_width if bar_width <= max_bar_width else max_bar_width)
+        self.completed = 0
+        if start:
+            self.start()
+
+    def _get_max_bar_width(self):
+        terminal_width, _ = get_terminal_size()
+        max_bar_width = min(int(terminal_width * 0.6), terminal_width - 50)
+        if max_bar_width < 10:
+            print('terminal width is too small ({}), please consider widen the terminal for better '
+                  'progressbar visualization'.format(terminal_width))
+            max_bar_width = 10
+        return max_bar_width
+
+    def start(self):
+        if self.task_num > 0:
+            sys.stdout.write('[{}] 0/{}, elapsed: 0s, ETA:\n{}\n'.format(
+                ' ' * self.bar_width, self.task_num, 'Start...'))
+        else:
+            sys.stdout.write('completed: 0, elapsed: 0s')
+        sys.stdout.flush()
+        self.start_time = time.time()
+
+    def update(self, msg='In progress...'):
+        self.completed += 1
+        elapsed = time.time() - self.start_time
+        fps = self.completed / elapsed
+        if self.task_num > 0:
+            percentage = self.completed / float(self.task_num)
+            eta = int(elapsed * (1 - percentage) / percentage + 0.5)
+            mark_width = int(self.bar_width * percentage)
+            bar_chars = '>' * mark_width + '-' * (self.bar_width - mark_width)
+            sys.stdout.write('\033[2F')  # cursor up 2 lines
+            sys.stdout.write('\033[J')  # clean the output (remove extra chars since last display)
+            sys.stdout.write('[{}] {}/{}, {:.1f} task/s, elapsed: {}s, ETA: {:5}s\n{}\n'.format(
+                bar_chars, self.completed, self.task_num, fps, int(elapsed + 0.5), eta, msg))
+        else:
+            sys.stdout.write('completed: {}, elapsed: {}s, {:.1f} tasks/s'.format(
+                self.completed, int(elapsed + 0.5), fps))
+        sys.stdout.flush()
