99d08d749baa0b500ec8710c8bee6fb2e443230c
XintaoWang <wxt1994@126.com>
2019-05-30 23:29:23 +0800

    update train.py

--- codes/train.py
+++ codes/train.py
@@ -1,89 +1,153 @@
-import os.path
+import os
+import os.path as osp
 import sys
 import math
 import argparse
 import time
 import random
-import numpy as np
-from collections import OrderedDict
 import logging
+from collections import OrderedDict
+import numpy as np
 
 import torch
+import torch.distributed as dist
+import torch.multiprocessing as mp
+from data.data_sampler import DistIterSampler
 
 import options.options as option
 from utils import util
 from data import create_dataloader, create_dataset
 from models import create_model
 
+import yaml
+
+
+def init_dist(backend='nccl', **kwargs):
+    ''' initialization for distributed training'''
+    # if mp.get_start_method(allow_none=True) is None:
+    if mp.get_start_method(allow_none=True) != 'spawn':
+        mp.set_start_method('spawn')
+    rank = int(os.environ['RANK'])
+    num_gpus = torch.cuda.device_count()
+    torch.cuda.set_device(rank % num_gpus)
+    dist.init_process_group(backend=backend, **kwargs)
+
 
 def main():
-    # options
+    #### options
     parser = argparse.ArgumentParser()
-    parser.add_argument('-opt', type=str, required=True, help='Path to option JSON file.')
-    opt = option.parse(parser.parse_args().opt, is_train=True)
-    opt = option.dict_to_nonedict(opt)  # Convert to NoneDict, which return None for missing key.
+    parser.add_argument('-opt', type=str, help='Path to option JSON file.')
+    parser.add_argument(
+        '--launcher', choices=['none', 'pytorch'], default='none', help='job launcher')
+    parser.add_argument('--local_rank', type=int, default=0)
+    args = parser.parse_args()
+    opt = option.parse(args.opt, is_train=True)
 
-    # train from scratch OR resume training
-    if opt['path']['resume_state']:  # resuming training
-        resume_state = torch.load(opt['path']['resume_state'])
-    else:  # training from scratch
+    #### distributed training settings
+    if args.launcher == 'none':  # disabled distributed training
+        opt['dist'] = False
+        rank = -1
+        print('Disabled distributed training.')
+    else:
+        opt['dist'] = True
+        init_dist()
+        world_size = torch.distributed.get_world_size()
+        rank = torch.distributed.get_rank()
+
+    #### loading resume state if exists
+    if opt['path'].get('resume_state', None):
+        # distributed resuming: all load into default GPU
+        device_id = torch.cuda.current_device()
+        resume_state = torch.load(
+            opt['path']['resume_state'], map_location=lambda storage, loc: storage.cuda(device_id))
+        option.check_resume(opt, resume_state['iter'])  # check resume options
+    else:
         resume_state = None
-        util.mkdir_and_rename(opt['path']['experiments_root'])  # rename old folder if exists
-        util.mkdirs((path for key, path in opt['path'].items() if not key == 'experiments_root'
-                     and 'pretrain_model' not in key and 'resume' not in key))
 
-    # config loggers. Before it, the log will not work
-    util.setup_logger(None, opt['path']['log'], 'train', level=logging.INFO, screen=True)
-    util.setup_logger('val', opt['path']['log'], 'val', level=logging.INFO)
-    logger = logging.getLogger('base')
+    #### mkdir and loggers
+    if rank <= 0:  # normal training (rank -1) OR distributed training (rank 0)
+        if resume_state is None:
+            util.mkdir_and_rename(
+                opt['path']['experiments_root'])  # rename experiment folder if exists
+            util.mkdirs((path for key, path in opt['path'].items() if not key == 'experiments_root'
+                         and 'pretrain_model' not in key and 'resume' not in key))
 
-    if resume_state:
-        logger.info('Resuming training from epoch: {}, iter: {}.'.format(
-            resume_state['epoch'], resume_state['iter']))
-        option.check_resume(opt)  # check resume options
+        # config loggers. Before it, the log will not work
+        util.setup_logger(
+            'base',
+            opt['path']['log'],
+            'train_' + opt['name'],
+            level=logging.INFO,
+            screen=True,
+            tofile=True)
+        util.setup_logger(
+            'val',
+            opt['path']['log'],
+            'val_' + opt['name'],
+            level=logging.INFO,
+            screen=True,
+            tofile=True)
+        logger = logging.getLogger('base')
+        logger.info(option.dict2str(opt))
+        # tensorboard logger
+        if opt['use_tb_logger'] and 'debug' not in opt['name']:
+            from tensorboardX import SummaryWriter
+            tb_logger = SummaryWriter(log_dir='../tb_logger/' + opt['name'])
+    else:
+        util.setup_logger('base', opt['path']['log'], 'train', level=logging.INFO, screen=True)
+        logger = logging.getLogger('base')
 
-    logger.info(option.dict2str(opt))
-    # tensorboard logger
-    if opt['use_tb_logger'] and 'debug' not in opt['name']:
-        from tensorboardX import SummaryWriter
-        tb_logger = SummaryWriter(log_dir='../tb_logger/' + opt['name'])
+    # convert to NoneDict, which returns None for missing keys
+    opt = option.dict_to_nonedict(opt)
 
-    # random seed
+    #### random seed
     seed = opt['train']['manual_seed']
     if seed is None:
         seed = random.randint(1, 10000)
-    logger.info('Random seed: {}'.format(seed))
+    if rank <= 0:
+        logger.info('Random seed: {}'.format(seed))
     util.set_random_seed(seed)
 
     torch.backends.cudnn.benckmark = True
     # torch.backends.cudnn.deterministic = True
 
-    # create train and val dataloader
+    #### create train and val dataloader
+    dataset_ratio = 200  # enlarge the size of each epoch
     for phase, dataset_opt in opt['datasets'].items():
         if phase == 'train':
             train_set = create_dataset(dataset_opt)
             train_size = int(math.ceil(len(train_set) / dataset_opt['batch_size']))
-            logger.info('Number of train images: {:,d}, iters: {:,d}'.format(
-                len(train_set), train_size))
             total_iters = int(opt['train']['niter'])
             total_epochs = int(math.ceil(total_iters / train_size))
-            logger.info('Total epochs needed: {:d} for iters {:,d}'.format(
-                total_epochs, total_iters))
-            train_loader = create_dataloader(train_set, dataset_opt)
+            if opt['dist']:
+                train_sampler = DistIterSampler(train_set, world_size, rank, dataset_ratio)
+                total_epochs = int(math.ceil(total_iters / (train_size * dataset_ratio)))
+            else:
+                train_sampler = None
+            train_loader = create_dataloader(train_set, dataset_opt, opt, train_sampler)
+            if rank <= 0:
+                logger.info('Number of train images: {:,d}, iters: {:,d}'.format(
+                    len(train_set), train_size))
+                logger.info('Total epochs needed: {:d} for iters {:,d}'.format(
+                    total_epochs, total_iters))
         elif phase == 'val':
             val_set = create_dataset(dataset_opt)
-            val_loader = create_dataloader(val_set, dataset_opt)
-            logger.info('Number of val images in [{:s}]: {:d}'.format(dataset_opt['name'],
-                                                                      len(val_set)))
+            val_loader = create_dataloader(val_set, dataset_opt, opt, None)
+            if rank <= 0:
+                logger.info('Number of val images in [{:s}]: {:d}'.format(
+                    dataset_opt['name'], len(val_set)))
         else:
             raise NotImplementedError('Phase [{:s}] is not recognized.'.format(phase))
     assert train_loader is not None
 
-    # create model
+    #### create model
     model = create_model(opt)
 
-    # resume training
+    #### resume training
     if resume_state:
+        logger.info('Resuming training from epoch: {}, iter: {}.'.format(
+            resume_state['epoch'], resume_state['iter']))
+
         start_epoch = resume_state['epoch']
         current_step = resume_state['iter']
         model.resume_training(resume_state)  # handle optimizers and schedulers
@@ -91,21 +155,23 @@ def main():
         current_step = 0
         start_epoch = 0
 
-    # training
+    #### training
     logger.info('Start training from epoch: {:d}, iter: {:d}'.format(start_epoch, current_step))
-    for epoch in range(start_epoch, total_epochs):
+    for epoch in range(start_epoch, total_epochs + 1):
+        if opt['dist']:
+            train_sampler.set_epoch(epoch)
         for _, train_data in enumerate(train_loader):
             current_step += 1
             if current_step > total_iters:
                 break
-            # update learning rate
-            model.update_learning_rate()
+            #### update learning rate
+            model.update_learning_rate(current_step, warmup_iter=opt['train']['warmup_iter'])
 
-            # training
+            #### training
             model.feed_data(train_data)
             model.optimize_parameters(current_step)
 
-            # log
+            #### log
             if current_step % opt['logger']['print_freq'] == 0:
                 logs = model.get_current_log()
                 message = '<epoch:{:3d}, iter:{:8,d}, lr:{:.3e}> '.format(
@@ -114,11 +180,13 @@ def main():
                     message += '{:s}: {:.4e} '.format(k, v)
                     # tensorboard logger
                     if opt['use_tb_logger'] and 'debug' not in opt['name']:
-                        tb_logger.add_scalar(k, v, current_step)
-                logger.info(message)
+                        if rank <= 0:
+                            tb_logger.add_scalar(k, v, current_step)
+                if rank <= 0:
+                    logger.info(message)
 
             # validation
-            if current_step % opt['train']['val_freq'] == 0:
+            if current_step % opt['train']['val_freq'] == 0 and rank <= 0:
                 avg_psnr = 0.0
                 idx = 0
                 for val_data in val_loader:
@@ -158,15 +226,17 @@ def main():
                 if opt['use_tb_logger'] and 'debug' not in opt['name']:
                     tb_logger.add_scalar('psnr', avg_psnr, current_step)
 
-            # save models and training states
+            #### save models and training states
             if current_step % opt['logger']['save_checkpoint_freq'] == 0:
-                logger.info('Saving models and training states.')
-                model.save(current_step)
-                model.save_training_state(epoch, current_step)
+                if rank <= 0:
+                    logger.info('Saving models and training states.')
+                    model.save(current_step)
+                    model.save_training_state(epoch, current_step)
 
-    logger.info('Saving the final model.')
-    model.save('latest')
-    logger.info('End of training.')
+    if rank <= 0:
+        logger.info('Saving the final model.')
+        model.save('latest')
+        logger.info('End of training.')
 
 
 if __name__ == '__main__':
