c6b92aab3928c587b6ea9855956f7c95a03f9338
XintaoWang <wxt1994@126.com>
2019-06-01 22:44:55 +0800

    update base_model
    Former-commit-id: bc9a9ebb8bba8a6b2865fc7eed4c00e6f6b1693e

--- codes/models/base_model.py
+++ codes/models/base_model.py
@@ -1,6 +1,8 @@
 import os
+from collections import OrderedDict
 import torch
 import torch.nn as nn
+from torch.nn.parallel import DistributedDataParallel
 
 
 class BaseModel():
@@ -32,25 +34,50 @@ class BaseModel():
     def load(self):
         pass
 
-    def update_learning_rate(self):
+    def _set_lr(self, lr_groups_l):
+        ''' set learning rate for warmup,
+        lr_groups_l: list for lr_groups. each for a optimizer'''
+        for optimizer, lr_groups in zip(self.optimizers, lr_groups_l):
+            for param_group, lr in zip(optimizer.param_groups, lr_groups):
+                param_group['lr'] = lr
+
+    def _get_init_lr(self):
+        # get the initial lr, which is set by the scheduler
+        init_lr_groups_l = []
+        for optimizer in self.optimizers:
+            init_lr_groups_l.append([v['initial_lr'] for v in optimizer.param_groups])
+        return init_lr_groups_l
+
+    def update_learning_rate(self, cur_iter, warmup_iter=-1):
         for scheduler in self.schedulers:
             scheduler.step()
+        #### set up warm up learning rate
+        if cur_iter < warmup_iter:
+            # get initial lr for each group
+            init_lr_g_l = self._get_init_lr()
+            # modify warming-up learning rates
+            warm_up_lr_l = []
+            for init_lr_g in init_lr_g_l:
+                warm_up_lr_l.append([v / warmup_iter * cur_iter for v in init_lr_g])
+            # set learning rate
+            self._set_lr(warm_up_lr_l)
 
     def get_current_learning_rate(self):
-        return self.schedulers[0].get_lr()[0]
+        # return self.schedulers[0].get_lr()[0]
+        return self.optimizers[0].param_groups[0]['lr']
 
     def get_network_description(self, network):
         '''Get the string and total parameters of the network'''
-        if isinstance(network, nn.DataParallel):
+        if isinstance(network, nn.DataParallel) or isinstance(network, DistributedDataParallel):
             network = network.module
         s = str(network)
         n = sum(map(lambda x: x.numel(), network.parameters()))
         return s, n
 
-    def save_network(self, network, network_label, iter_step):
-        save_filename = '{}_{}.pth'.format(iter_step, network_label)
+    def save_network(self, network, network_label, iter_label):
+        save_filename = '{}_{}.pth'.format(iter_label, network_label)
         save_path = os.path.join(self.opt['path']['models'], save_filename)
-        if isinstance(network, nn.DataParallel):
+        if isinstance(network, nn.DataParallel) or isinstance(network, DistributedDataParallel):
             network = network.module
         state_dict = network.state_dict()
         for key, param in state_dict.items():
@@ -58,9 +85,16 @@ class BaseModel():
         torch.save(state_dict, save_path)
 
     def load_network(self, load_path, network, strict=True):
-        if isinstance(network, nn.DataParallel):
+        if isinstance(network, nn.DataParallel) or isinstance(network, DistributedDataParallel):
             network = network.module
-        network.load_state_dict(torch.load(load_path), strict=strict)
+        load_net = torch.load(load_path)
+        load_net_clean = OrderedDict()  # remove unnecessary 'module.'
+        for k, v in load_net.items():
+            if k.startswith('module.'):
+                load_net_clean[k[7:]] = v
+            else:
+                load_net_clean[k] = v
+        network.load_state_dict(load_net_clean, strict=strict)
 
     def save_training_state(self, epoch, iter_step):
         '''Saves training state during training, which will be used for resuming'''
