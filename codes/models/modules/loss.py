02b4ef4c211981841d782f49c450ec64e6fb9eb5
XintaoWang <wxt1994@126.com>
2019-06-01 22:42:34 +0800

    add CharbonnierLoss
    Former-commit-id: c7a281f2d75943e715dc0ac1c84e1b186ae35158

--- codes/models/modules/loss.py
+++ codes/models/modules/loss.py
@@ -2,6 +2,19 @@ import torch
 import torch.nn as nn
 
 
+class CharbonnierLoss(nn.Module):
+    """Charbonnier Loss (L1)"""
+
+    def __init__(self, eps=1e-6):
+        super(CharbonnierLoss, self).__init__()
+        self.eps = eps
+
+    def forward(self, x, y):
+        diff = x - y
+        loss = torch.sum(torch.sqrt(diff * diff + self.eps))
+        return loss
+
+
 # Define GAN loss: [vanilla | lsgan | wgan-gp]
 class GANLoss(nn.Module):
     def __init__(self, gan_type, real_label_val=1.0, fake_label_val=0.0):
