4ee757e57701e88ab8aa2cd9e90d8dde7b93a4e6
XintaoWang <wxt1994@126.com>
2019-06-01 22:02:01 +0800

    update data/__init__ and LRHR_dataset

--- codes/data/LRHR_dataset.py
+++ codes/data/LRHR_dataset.py
@@ -1,4 +1,3 @@
-import os.path
 import random
 import numpy as np
 import cv2
@@ -9,52 +8,43 @@ import data.util as util
 
 class LRHRDataset(data.Dataset):
     '''
-    Read LR and HR image pairs.
-    If only HR image is provided, generate LR image on-the-fly.
+    Read LR and GT image pairs.
+    If only GT image is provided, generate LR image on-the-fly.
     The pair is ensured by 'sorted' function, so please check the name convention.
     '''
 
     def __init__(self, opt):
         super(LRHRDataset, self).__init__()
         self.opt = opt
-        self.paths_LR = None
-        self.paths_HR = None
-        self.LR_env = None  # environment for lmdb
-        self.HR_env = None
+        self.paths_LR, self.paths_GT = None, None
+        self.LR_env, self.GT_env = None, None  # environment for lmdb
 
-        # read image list from subset list txt
-        if opt['subset_file'] is not None and opt['phase'] == 'train':
-            with open(opt['subset_file']) as f:
-                self.paths_HR = sorted([os.path.join(opt['dataroot_HR'], line.rstrip('\n')) \
-                        for line in f])
-            if opt['dataroot_LR'] is not None:
-                raise NotImplementedError('Now subset only supports generating LR on-the-fly.')
-        else:  # read image list from lmdb or image files
-            self.HR_env, self.paths_HR = util.get_image_paths(opt['data_type'], opt['dataroot_HR'])
-            self.LR_env, self.paths_LR = util.get_image_paths(opt['data_type'], opt['dataroot_LR'])
+        # read image list from lmdb or image files
+        self.GT_env, self.paths_GT = util.get_image_paths(opt['data_type'], opt['dataroot_GT'])
+        self.LR_env, self.paths_LR = util.get_image_paths(opt['data_type'], opt['dataroot_LR'])
 
-        assert self.paths_HR, 'Error: HR path is empty.'
-        if self.paths_LR and self.paths_HR:
-            assert len(self.paths_LR) == len(self.paths_HR), \
-                'HR and LR datasets have different number of images - {}, {}.'.format(\
-                len(self.paths_LR), len(self.paths_HR))
+        assert self.paths_GT, 'Error: GT path is empty.'
+        if self.paths_LR and self.paths_GT:
+            assert len(self.paths_LR) == len(self.paths_GT), \
+                'GT and LR datasets have different number of images - {}, {}.'.format(\
+                len(self.paths_LR), len(self.paths_GT))
 
         self.random_scale_list = [1]
 
     def __getitem__(self, index):
-        HR_path, LR_path = None, None
+        GT_path, LR_path = None, None
         scale = self.opt['scale']
-        HR_size = self.opt['HR_size']
+        GT_size = self.opt['GT_size']
 
-        # get HR image
-        HR_path = self.paths_HR[index]
-        img_HR = util.read_img(self.HR_env, HR_path)
+        # get GT image
+        GT_path = self.paths_GT[index]
+        img_GT = util.read_img(self.GT_env, GT_path)
         # modcrop in the validation / test phase
         if self.opt['phase'] != 'train':
-            img_HR = util.modcrop(img_HR, scale)
+            img_GT = util.modcrop(img_GT, scale)
         # change color space if necessary
         if self.opt['color']:
-            img_HR = util.channel_convert(img_HR.shape[2], self.opt['color'], [img_HR])[0]
+            img_GT = util.channel_convert(img_GT.shape[2], self.opt['color'], [img_GT])[0]
 
         # get LR image
         if self.paths_LR:
@@ -64,65 +54,66 @@ class LRHRDataset(data.Dataset):
             # randomly scale during training
             if self.opt['phase'] == 'train':
                 random_scale = random.choice(self.random_scale_list)
-                H_s, W_s, _ = img_HR.shape
+                H_s, W_s, _ = img_GT.shape
 
                 def _mod(n, random_scale, scale, thres):
                     rlt = int(n * random_scale)
                     rlt = (rlt // scale) * scale
                     return thres if rlt < thres else rlt
 
-                H_s = _mod(H_s, random_scale, scale, HR_size)
-                W_s = _mod(W_s, random_scale, scale, HR_size)
-                img_HR = cv2.resize(np.copy(img_HR), (W_s, H_s), interpolation=cv2.INTER_LINEAR)
+                H_s = _mod(H_s, random_scale, scale, GT_size)
+                W_s = _mod(W_s, random_scale, scale, GT_size)
+                img_GT = cv2.resize(np.copy(img_GT), (W_s, H_s), interpolation=cv2.INTER_LINEAR)
                 # force to 3 channels
-                if img_HR.ndim == 2:
-                    img_HR = cv2.cvtColor(img_HR, cv2.COLOR_GRAY2BGR)
+                if img_GT.ndim == 2:
+                    img_GT = cv2.cvtColor(img_GT, cv2.COLOR_GRAY2BGR)
 
-            H, W, _ = img_HR.shape
+            H, W, _ = img_GT.shape
             # using matlab imresize
-            img_LR = util.imresize_np(img_HR, 1 / scale, True)
+            img_LR = util.imresize_np(img_GT, 1 / scale, True)
             if img_LR.ndim == 2:
                 img_LR = np.expand_dims(img_LR, axis=2)
 
         if self.opt['phase'] == 'train':
             # if the image size is too small
-            H, W, _ = img_HR.shape
-            if H < HR_size or W < HR_size:
-                img_HR = cv2.resize(
-                    np.copy(img_HR), (HR_size, HR_size), interpolation=cv2.INTER_LINEAR)
+            H, W, _ = img_GT.shape
+            if H < GT_size or W < GT_size:
+                img_GT = cv2.resize(
+                    np.copy(img_GT), (GT_size, GT_size), interpolation=cv2.INTER_LINEAR)
                 # using matlab imresize
-                img_LR = util.imresize_np(img_HR, 1 / scale, True)
+                img_LR = util.imresize_np(img_GT, 1 / scale, True)
                 if img_LR.ndim == 2:
                     img_LR = np.expand_dims(img_LR, axis=2)
 
             H, W, C = img_LR.shape
-            LR_size = HR_size // scale
+            LR_size = GT_size // scale
 
             # randomly crop
             rnd_h = random.randint(0, max(0, H - LR_size))
             rnd_w = random.randint(0, max(0, W - LR_size))
             img_LR = img_LR[rnd_h:rnd_h + LR_size, rnd_w:rnd_w + LR_size, :]
-            rnd_h_HR, rnd_w_HR = int(rnd_h * scale), int(rnd_w * scale)
-            img_HR = img_HR[rnd_h_HR:rnd_h_HR + HR_size, rnd_w_HR:rnd_w_HR + HR_size, :]
+            rnd_h_GT, rnd_w_GT = int(rnd_h * scale), int(rnd_w * scale)
+            img_GT = img_GT[rnd_h_GT:rnd_h_GT + GT_size, rnd_w_GT:rnd_w_GT + GT_size, :]
 
             # augmentation - flip, rotate
-            img_LR, img_HR = util.augment([img_LR, img_HR], self.opt['use_flip'], \
+            img_LR, img_GT = util.augment([img_LR, img_GT], self.opt['use_flip'], \
                 self.opt['use_rot'])
 
         # change color space if necessary
         if self.opt['color']:
-            img_LR = util.channel_convert(C, self.opt['color'], [img_LR])[0] # TODO during val no definetion
+            img_LR = util.channel_convert(C, self.opt['color'],
+                                          [img_LR])[0]  # TODO during val no definition
 
         # BGR to RGB, HWC to CHW, numpy to tensor
-        if img_HR.shape[2] == 3:
-            img_HR = img_HR[:, :, [2, 1, 0]]
+        if img_GT.shape[2] == 3:
+            img_GT = img_GT[:, :, [2, 1, 0]]
             img_LR = img_LR[:, :, [2, 1, 0]]
-        img_HR = torch.from_numpy(np.ascontiguousarray(np.transpose(img_HR, (2, 0, 1)))).float()
+        img_GT = torch.from_numpy(np.ascontiguousarray(np.transpose(img_GT, (2, 0, 1)))).float()
         img_LR = torch.from_numpy(np.ascontiguousarray(np.transpose(img_LR, (2, 0, 1)))).float()
 
         if LR_path is None:
-            LR_path = HR_path
-        return {'LR': img_LR, 'HR': img_HR, 'LR_path': LR_path, 'HR_path': HR_path}
+            LR_path = GT_path
+        return {'LR': img_LR, 'GT': img_GT, 'LR_path': LR_path, 'GT_path': GT_path}
 
     def __len__(self):
-        return len(self.paths_HR)
+        return len(self.paths_GT)
--- codes/data/__init__.py
+++ codes/data/__init__.py
@@ -1,36 +1,49 @@
 '''create dataset and dataloader'''
 import logging
+import torch
 import torch.utils.data
 
 
-def create_dataloader(dataset, dataset_opt):
-    '''create dataloader '''
+def create_dataloader(dataset, dataset_opt, opt, sampler):
+    if opt['dist']:
+        world_size = torch.distributed.get_world_size()
+        rank = torch.distributed.get_rank()
+        num_workers = dataset_opt['n_workers']
+        assert dataset_opt['batch_size'] % world_size == 0
+        batch_size = dataset_opt['batch_size'] // world_size
+        shuffle = False
+    else:
+        num_workers = dataset_opt['n_workers'] * len(opt['gpu_ids'])
+        batch_size = dataset_opt['batch_size']
+        shuffle = True
+
     phase = dataset_opt['phase']
     if phase == 'train':
         return torch.utils.data.DataLoader(
             dataset,
-            batch_size=dataset_opt['batch_size'],
-            shuffle=dataset_opt['use_shuffle'],
-            num_workers=dataset_opt['n_workers'],
+            batch_size=batch_size,
+            shuffle=shuffle,
+            num_workers=num_workers,
+            sampler=sampler,
             drop_last=True,
-            pin_memory=True)
+            pin_memory=False)
     else:
         return torch.utils.data.DataLoader(
             dataset, batch_size=1, shuffle=False, num_workers=1, pin_memory=True)
 
 
 def create_dataset(dataset_opt):
-    '''create dataset'''
     mode = dataset_opt['mode']
     if mode == 'LR':
         from data.LR_dataset import LRDataset as D
     elif mode == 'LRHR':
         from data.LRHR_dataset import LRHRDataset as D
-    elif mode == 'LRHRseg_bg':
-        from data.LRHR_seg_bg_dataset import LRHRSeg_BG_Dataset as D
+    # elif mode == 'LRHRseg_bg':
+    #     from data.LRHR_seg_bg_dataset import LRHRSeg_BG_Dataset as D
     else:
         raise NotImplementedError('Dataset [{:s}] is not recognized.'.format(mode))
     dataset = D(dataset_opt)
+
     logger = logging.getLogger('base')
     logger.info('Dataset [{:s} - {:s}] is created.'.format(dataset.__class__.__name__,
                                                            dataset_opt['name']))
