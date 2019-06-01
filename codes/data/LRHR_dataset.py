c2ab8158869d7079276f204c8e12c5c417172cc2
XintaoWang <wxt1994@126.com>
2019-06-01 23:53:09 +0800

    fix bug dataloader

--- codes/data/LRHR_dataset.py
+++ codes/data/LRHR_dataset.py
@@ -1,6 +1,9 @@
+import os.path as osp
 import random
+import pickle
 import numpy as np
 import cv2
+import lmdb
 import torch
 import torch.utils.data as data
 import data.util as util
@@ -16,22 +19,35 @@ class LRHRDataset(data.Dataset):
     def __init__(self, opt):
         super(LRHRDataset, self).__init__()
         self.opt = opt
+        self.data_type = self.opt['data_type']
         self.paths_LR, self.paths_GT = None, None
         self.LR_env, self.GT_env = None, None  # environment for lmdb
-
-        # read image list from lmdb or image files
-        self.GT_env, self.paths_GT = util.get_image_paths(opt['data_type'], opt['dataroot_GT'])
-        self.LR_env, self.paths_LR = util.get_image_paths(opt['data_type'], opt['dataroot_LR'])
-
+        if self.data_type == 'img':
+            self.paths_GT = sorted(util._get_paths_from_images(opt['dataroot_GT']))
+            self.paths_LR = sorted(util._get_paths_from_images(opt['dataroot_LR']))
+        elif self.data_type == 'lmdb':
+            keys = pickle.load(open(osp.join(opt['dataroot_GT'], '_keys_cache.p'), 'rb'))
+            self.paths_GT = sorted([key for key in keys if not key.endswith('.meta')])
+            keys = pickle.load(open(osp.join(opt['dataroot_LR'], '_keys_cache.p'), 'rb'))
+            self.paths_LR = sorted([key for key in keys if not key.endswith('.meta')])
         assert self.paths_GT, 'Error: GT path is empty.'
         if self.paths_LR and self.paths_GT:
             assert len(self.paths_LR) == len(self.paths_GT), \
                 'GT and LR datasets have different number of images - {}, {}.'.format(\
                 len(self.paths_LR), len(self.paths_GT))
-
         self.random_scale_list = [1]
 
+    def _init_lmdb(self):
+        # https://github.com/chainer/chainermn/issues/129
+        self.GT_env = lmdb.open(
+            self.opt['dataroot_GT'], readonly=True, lock=False, readahead=False, meminit=False)
+        self.LR_env = lmdb.open(
+            self.opt['dataroot_LR'], readonly=True, lock=False, readahead=False, meminit=False)
+
     def __getitem__(self, index):
+        if self.data_type == 'lmdb':
+            if (self.GT_env is None) or (self.LR_env is None):
+                self._init_lmdb()
         GT_path, LR_path = None, None
         scale = self.opt['scale']
         GT_size = self.opt['GT_size']
