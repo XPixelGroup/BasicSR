c6164c444c458861223d49b04d3420c2ad8f7571
XintaoWang <wxt1994@126.com>
2019-06-02 22:57:32 +0800

    update create_lmdb

--- codes/scripts/create_lmdb.py
+++ codes/scripts/create_lmdb.py
@@ -1,40 +1,42 @@
+import os
+import os.path as osp
 import sys
-import os.path
 import glob
 import pickle
 import lmdb
 import cv2
 
-sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
-from utils.progress_bar import ProgressBar
+sys.path.append(osp.dirname(osp.dirname(osp.abspath(__file__))))
+from utils.util import ProgressBar
 
 # configurations
-img_folder = '/mnt/SSD/xtwang/BasicSR_datasets/DIV2K800/DIV2K800/*'  # glob matching pattern
-lmdb_save_path = '/mnt/SSD/xtwang/BasicSR_datasets/DIV2K800/DIV2K800.lmdb'  # must end with .lmdb
-mode = 1 # 1 for small data (more memory), 2 for large data (less memory)
+img_folder = '/mnt/SSD/xtwang/BasicSR_datasets/DIV2K800/DIV2K800_sub/*'  # glob matching pattern
+lmdb_save_path = '/mnt/SSD/xtwang/BasicSR_datasets/DIV2K800/DIV2K800_subV2.lmdb'  # must end with .lmdb
+meta_info = {'name': 'DIV2K800_sub_GT'}
+mode = 2  # 1 for reading all the images to memory and then writing to lmdb (more memory);
+# 2 for reading several images and then writing to lmdb, loop over (less memory)
+batch = 1000  # Used in mode 2. After batch images, lmdb commits.
+###########################################
 
 img_list = sorted(glob.glob(img_folder))
-
-print('Read images...')
-# mode 1 small data, read all imgs
 if mode == 1:
+    print('Read images...')
     dataset = [cv2.imread(v, cv2.IMREAD_UNCHANGED) for v in img_list]
     data_size = sum([img.nbytes for img in dataset])
-# mode 2 large data, read imgs later
 elif mode == 2:
+    print('Calculating the total size of images...')
     data_size = sum(os.stat(v).st_size for v in img_list)
 else:
     raise ValueError('mode should be 1 or 2')
 
-env = lmdb.open(lmdb_save_path, map_size=data_size * 10)
-print('Finish reading {} images.\nWrite lmdb...'.format(len(img_list)))
-
+key_l = []
+resolution_l = []
 pbar = ProgressBar(len(img_list))
-batch = 3000   # can be modified according to memory usage
-txn = env.begin(write=True) # txn is a Transaction object
+env = lmdb.open(lmdb_save_path, map_size=data_size * 10)
+txn = env.begin(write=True)  # txn is a Transaction object
 for i, v in enumerate(img_list):
     pbar.update('Write {}'.format(v))
-    base_name = os.path.splitext(os.path.basename(v))[0]
+    base_name = osp.splitext(osp.basename(v))[0]
     key = base_name.encode('ascii')
     data = dataset[i] if mode == 1 else cv2.imread(v, cv2.IMREAD_UNCHANGED)
     if data.ndim == 2:
@@ -42,12 +44,11 @@ for i, v in enumerate(img_list):
         C = 1
     else:
         H, W, C = data.shape
-    meta_key = (base_name + '.meta').encode('ascii')
-    meta = '{:d}, {:d}, {:d}'.format(H, W, C)
-    # The encode is only essential in Python 3
     txn.put(key, data)
-    txn.put(meta_key, meta.encode('ascii'))
-    if mode == 2 and i % batch == batch - 1:
+    key_l.append(base_name)
+    resolution_l.append('{:d}_{:d}_{:d}'.format(C, H, W))
+    # commit in mode 2
+    if mode == 2 and i % batch == 1:
         txn.commit()
         txn = env.begin(write=True)
 
@@ -56,11 +57,19 @@ env.close()
 
 print('Finish writing lmdb.')
 
-# create keys cache
-keys_cache_file = os.path.join(lmdb_save_path, '_keys_cache.p')
-env = lmdb.open(lmdb_save_path, readonly=True, lock=False, readahead=False, meminit=False)
-with env.begin(write=False) as txn:
-    print('Create lmdb keys cache: {}'.format(keys_cache_file))
-    keys = [key.decode('ascii') for key, _ in txn.cursor()]
-    pickle.dump(keys, open(keys_cache_file, "wb"))
-print('Finish creating lmdb keys cache.')
+#### create meta information
+# check whether all the images are the same size
+same_resolution = (len(set(resolution_l)) <= 1)
+if same_resolution:
+    meta_info['resolution'] = [resolution_l[0]]
+    meta_info['keys'] = key_l
+    print('All images have the same resolution. Simplify the meta info...')
+else:
+    meta_info['resolution'] = resolution_l
+    meta_info['keys'] = key_l
+    print('Not all images have the same resolution. Save meta info for each image...')
+
+#### pickle dump
+keys_cache_file = osp.join(lmdb_save_path, 'meta_info.pkl')
+pickle.dump(meta_info, open(keys_cache_file, "wb"))
+print('Finish creating lmdb meta info.')
