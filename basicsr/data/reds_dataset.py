1536688c499f52a45c7958639231a3fa534facae
XintaoWang <wxt1994@126.com>
2020-06-25 16:01:57 +0800

    add import torch.utils.data as data

--- basicsr/data/reds_dataset.py
+++ basicsr/data/reds_dataset.py
@@ -4,6 +4,7 @@ from pathlib import Path
 import numpy as np
 import torch
 import mmcv
+import torch.utils.data as data
 
 from basicsr.data.transforms import augment, paired_random_crop, totensor
 from basicsr.utils import FileClient, get_root_logger
