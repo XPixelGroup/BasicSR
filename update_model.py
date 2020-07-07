a7e34e8cad8ebe9219e20e7882723e8f1ab8ae37
XintaoWang <wxt1994@126.com>
2020-07-07 20:56:02 +0800

    update

--- update_model.py
+++ update_model.py
@@ -1,6 +1,6 @@
 import glob
-import os.path as osp
 import subprocess
+from os import path as osp
 
 import torch
 
@@ -10,8 +10,11 @@ for path in paths:
     model_basename = osp.basename(path).split('-')[0]
     net = torch.load(path)
     out_file = path.split('-')[0]
-    torch.save(dict(params=net), out_file)
+    if 'params' in net:
+        pass
+    else:
+        torch.save(dict(params=net), out_file)
 
-    sha = subprocess.check_output(['sha256sum', out_file]).decode()
-    final_file = out_file.rstrip('.pth') + f'-{sha[:8]}.pth'
-    subprocess.Popen(['mv', out_file, final_file])
+        sha = subprocess.check_output(['sha256sum', out_file]).decode()
+        final_file = out_file.rstrip('.pth') + f'-{sha[:8]}.pth'
+        subprocess.Popen(['mv', out_file, final_file])
