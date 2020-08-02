66ee478bafa85efdde6f55969ff893d6e8b71945
Xintao <wxt1994@126.com>
2020-08-03 03:00:18 +0800

    Merge commits from BasicSR-private (#263)
    * add get_bare_model
    
    * add ffhq dataset
    
    * rm NoneDict
    
    * add test_ffhq_dataset
    
    * update train test commands
    
    Former-commit-id: 65b797b419eca78029d954fb405932da3f7a167f

---
+++ tests/test_ffhq_dataset.py
@@ -0,0 +1,60 @@
+import math
+import mmcv
+import torch
+import torchvision.utils
+
+from basicsr.data import create_dataloader, create_dataset
+
+
+def main(mode='folder'):
+    """Test FFHQ dataset."""
+    opt = {}
+    opt['dist'] = False
+    opt['gpu_ids'] = [0]
+    opt['phase'] = 'train'
+
+    opt['name'] = 'FFHQ'
+    opt['type'] = 'FFHQDataset'
+
+    opt['dataroot_gt'] = 'datasets/ffhq/ffhq_256.lmdb'
+    opt['io_backend'] = dict(type='lmdb')
+
+    opt['use_hflip'] = True
+    opt['mean'] = [0.5, 0.5, 0.5]
+    opt['std'] = [0.5, 0.5, 0.5]
+
+    opt['use_shuffle'] = True
+    opt['num_worker'] = 1
+    opt['batch_size'] = 4
+
+    opt['dataset_enlarge_ratio'] = 1
+
+    mmcv.mkdir_or_exist('tmp')
+
+    dataset = create_dataset(opt)
+    data_loader = create_dataloader(dataset, opt, opt, None)
+
+    nrow = int(math.sqrt(opt['batch_size']))
+    padding = 2 if opt['phase'] == 'train' else 0
+
+    print('start...')
+    for i, data in enumerate(data_loader):
+        if i > 5:
+            break
+        print(i)
+
+        gt = data['gt']
+        print(torch.min(gt), torch.max(gt))
+        gt_path = data['gt_path']
+        print(gt_path)
+        torchvision.utils.save_image(
+            gt,
+            f'tmp/gt_{i:03d}.png',
+            nrow=nrow,
+            padding=padding,
+            normalize=True,
+            range=(-1, 1))
+
+
+if __name__ == '__main__':
+    main()
