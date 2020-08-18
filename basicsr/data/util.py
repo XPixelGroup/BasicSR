fa6c68f829192e98bab90e0d8130837538925164
Xintao <wxt1994@126.com>
2020-08-18 21:15:07 +0800

    update data util

--- basicsr/data/util.py
+++ basicsr/data/util.py
@@ -14,7 +14,7 @@ def read_img_seq(path, require_mod_crop=False, scale=1):
         path (list[str] | str): List of image paths or image folder path.
         require_mod_crop (bool): Require mod crop for each image.
             Default: False.
-        scale (int): Scale factor for mod_crop.
+        scale (int): Scale factor for mod_crop. Default: 1.
 
     Returns:
         Tensor: size (t, c, h, w), RGB, [0, 1].
@@ -33,18 +33,18 @@ def read_img_seq(path, require_mod_crop=False, scale=1):
 
 def generate_frame_indices(crt_idx,
                            max_frame_num,
-                           n_frames,
+                           num_frames,
                            padding='reflection'):
-    """Generate an index list for reading `n_frames` frames from a sequence
+    """Generate an index list for reading `num_frames` frames from a sequence
     of images.
 
     Args:
         crt_idx (int): Current center index.
         max_frame_num (int): Max number of the sequence of images (from 1).
-        n_frames (int): Reading n_frames frames.
+        num_frames (int): Reading num_frames frames.
         padding (str): Padding mode, one of
             'replicate' | 'reflection' | 'reflection_circle' | 'circle'
-            Examples: current_idx = 0, n_frames = 5
+            Examples: current_idx = 0, num_frames = 5
             The generated frame indices under different padding mode:
             replicate: [0, 0, 0, 1, 2]
             reflection: [2, 1, 0, 1, 2]
@@ -54,33 +54,33 @@ def generate_frame_indices(crt_idx,
     Returns:
         list[int]: A list of indices.
     """
-    assert n_frames % 2 == 1, 'n_frames should be an odd number.'
+    assert num_frames % 2 == 1, 'num_frames should be an odd number.'
     assert padding in ('replicate', 'reflection', 'reflection_circle',
                        'circle'), f'Wrong padding mode: {padding}.'
 
     max_frame_num = max_frame_num - 1  # start from 0
-    n_pad = n_frames // 2
+    num_pad = num_frames // 2
 
     indices = []
-    for i in range(crt_idx - n_pad, crt_idx + n_pad + 1):
+    for i in range(crt_idx - num_pad, crt_idx + num_pad + 1):
         if i < 0:
             if padding == 'replicate':
                 pad_idx = 0
             elif padding == 'reflection':
                 pad_idx = -i
             elif padding == 'reflection_circle':
-                pad_idx = crt_idx + n_pad - i
+                pad_idx = crt_idx + num_pad - i
             else:
-                pad_idx = n_frames + i
+                pad_idx = num_frames + i
         elif i > max_frame_num:
             if padding == 'replicate':
                 pad_idx = max_frame_num
             elif padding == 'reflection':
                 pad_idx = max_frame_num * 2 - i
             elif padding == 'reflection_circle':
-                pad_idx = (crt_idx - n_pad) - (i - max_frame_num)
+                pad_idx = (crt_idx - num_pad) - (i - max_frame_num)
             else:
-                pad_idx = i - n_frames
+                pad_idx = i - num_frames
         else:
             pad_idx = i
         indices.append(pad_idx)
