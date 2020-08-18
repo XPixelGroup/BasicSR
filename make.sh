23543fde5a8d8d6ee43f1b30e5c85cdf39aaaa6a
Xintao <wxt1994@126.com>
2020-08-18 13:44:59 +0800

    update setup

--- make.sh
+++ make.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-# You may need to modify the following paths before compiling.
+# You may need to modify the following paths before compiling
 CUDA_HOME=/usr/local/cuda \
 CUDNN_INCLUDE_DIR=/usr/local/cuda \
 CUDNN_LIB_DIR=/usr/local/cuda \
-python setup.py build develop
+python setup.py develop
