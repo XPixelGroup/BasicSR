d185498a23a405ee611048913116f19d6d06211e
Xintao <wxt1994@126.com>
2020-08-19 21:17:52 +0800

    update publish_models

--- scripts/publish_models.py
+++ scripts/publish_models.py
@@ -9,11 +9,10 @@ for path in paths:
     model_basename = osp.basename(path).split('-')[0]
     net = torch.load(path)
     out_file = path.split('-')[0]
-    if 'params' in net:
-        pass
+    if 'params' not in net:
+        print(f"Please check! Model {model_basename} does not have 'params'.")
     else:
         torch.save(dict(params=net), out_file)
-
         sha = subprocess.check_output(['sha256sum', out_file]).decode()
         final_file = out_file.rstrip('.pth') + f'-{sha[:8]}.pth'
         subprocess.Popen(['mv', out_file, final_file])
