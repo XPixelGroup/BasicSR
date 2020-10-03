5442f6578dc21ab121e513ba12f36103c6a0d957
Xintao <wxt1994@126.com>
2020-10-03 21:07:29 +0800

    update utils.util

--- basicsr/utils/util.py
+++ basicsr/utils/util.py
@@ -1,5 +1,3 @@
-import cv2
-import math
 import numpy as np
 import os
 import random
@@ -8,41 +6,22 @@ import time
 import torch
 from os import path as osp
 from shutil import get_terminal_size
-from torchvision.utils import make_grid
 
-from basicsr.utils import get_root_logger
 from basicsr.utils.dist_util import master_only
+from basicsr.utils.logger import get_root_logger
+
+
+def set_random_seed(seed):
+    """Set random seeds."""
+    random.seed(seed)
+    np.random.seed(seed)
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed(seed)
+    torch.cuda.manual_seed_all(seed)
 
 
 def get_time_str():
     return time.strftime('%Y%m%d_%H%M%S', time.localtime())
-
-
-def check_resume(opt, resume_iter):
-    """Check resume states and pretrain_network paths.
-
-    Args:
-        opt (dict): Options.
-        resume_iter (int): Resume iteration.
-    """
-    logger = get_root_logger()
-    if opt['path']['resume_state']:
-        # get all the networks
-        networks = [key for key in opt.keys() if key.startwith('network_')]
-        flag_pretrain = False
-        for network in networks:
-            if opt['path'].get(f'pretrain_{network}') is not None:
-                flag_pretrain = True
-        if flag_pretrain:
-            logger.warning(
-                'pretrain_network path will be ignored during resuming.')
-        # set pretrained model paths
-        for network in networks:
-            name = f'pretrain_{network}'
-            basename = network.replace('network_', '')
-            opt['path'][name] = osp.join(opt['path']['models'],
-                                         f'net_{basename}_{resume_iter}.pth')
-            logger.info(f"Set {name} to {opt['path'][name]}")
 
 
 def mkdir_and_rename(path):
@@ -70,116 +49,6 @@ def make_exp_dirs(opt):
     for key, path in path_opt.items():
         if 'pretrain_network' not in key and 'resume' not in key:
             os.makedirs(path, exist_ok=False)
-
-
-def imwrite(img, file_path, params=None, auto_mkdir=True):
-    """Write image to file.
-
-    Args:
-        img (ndarray): Image array to be written.
-        file_path (str): Image file path.
-        params (None or list): Same as opencv's :func:`imwrite` interface.
-        auto_mkdir (bool): If the parent folder of `file_path` does not exist,
-            whether to create it automatically.
-
-    Returns:
-        bool: Successful or not.
-    """
-    if auto_mkdir:
-        dir_name = osp.abspath(osp.dirname(file_path))
-        os.makedirs(dir_name, exist_ok=True)
-    return cv2.imwrite(file_path, img, params)
-
-
-def set_random_seed(seed):
-    """Set random seeds."""
-    random.seed(seed)
-    np.random.seed(seed)
-    torch.manual_seed(seed)
-    torch.cuda.manual_seed(seed)
-    torch.cuda.manual_seed_all(seed)
-
-
-def crop_border(imgs, crop_border):
-    """Crop borders of images.
-
-    Args:
-        imgs (list[ndarray] | ndarray): Images with shape (h, w, c).
-        crop_border (int): Crop border for each end of height and weight.
-
-    Returns:
-        list[ndarray]: Cropped images.
-    """
-    if crop_border == 0:
-        return imgs
-    else:
-        if isinstance(imgs, list):
-            return [
-                v[crop_border:-crop_border, crop_border:-crop_border, ...]
-                for v in imgs
-            ]
-        else:
-            return imgs[crop_border:-crop_border, crop_border:-crop_border,
-                        ...]
-
-
-def tensor2img(tensor, out_type=np.uint8, min_max=(0, 1)):
-    """Convert torch Tensors into image numpy arrays.
-
-    After clamping to [min, max], values will be normalized to [0, 1].
-
-    Args:
-        tensor (Tensor or list[Tensor]): Accept shapes:
-            1) 4D mini-batch Tensor of shape (B x 3/1 x H x W);
-            2) 3D Tensor of shape (3/1 x H x W);
-            3) 2D Tensor of shape (H x W).
-            Tensor channel should be in RGB order.
-        out_type (numpy type): output types. If ``np.uint8``, transform outputs
-            to uint8 type with range [0, 255]; otherwise, float type with
-            range [0, 1]. Default: ``np.uint8``.
-        min_max (tuple[int]): min and max values for clamp.
-
-    Returns:
-        (Tensor or list): 3D ndarray of shape (H x W x C) OR 2D ndarray of
-        shape (H x W). The channel order is BGR.
-    """
-    if not (torch.is_tensor(tensor) or
-            (isinstance(tensor, list)
-             and all(torch.is_tensor(t) for t in tensor))):
-        raise TypeError(
-            f'tensor or list of tensors expected, got {type(tensor)}')
-
-    if torch.is_tensor(tensor):
-        tensor = [tensor]
-    result = []
-    for _tensor in tensor:
-        _tensor = _tensor.squeeze(0).float().detach().cpu().clamp_(*min_max)
-        _tensor = (_tensor - min_max[0]) / (min_max[1] - min_max[0])
-
-        n_dim = _tensor.dim()
-        if n_dim == 4:
-            img_np = make_grid(
-                _tensor, nrow=int(math.sqrt(_tensor.size(0))),
-                normalize=False).numpy()
-            img_np = np.transpose(img_np[[2, 1, 0], :, :],
-                                  (1, 2, 0))  # HWC, BGR
-        elif n_dim == 3:
-            img_np = _tensor.numpy()
-            img_np = np.transpose(img_np[[2, 1, 0], :, :],
-                                  (1, 2, 0))  # HWC, BGR
-        elif n_dim == 2:
-            img_np = _tensor.numpy()
-        else:
-            raise TypeError('Only support 4D, 3D or 2D tensor. '
-                            f'But received with dimension: {n_dim}')
-        if out_type == np.uint8:
-            # Unlike MATLAB, numpy.unit8() WILL NOT round by default.
-            img_np = (img_np * 255.0).round()
-        img_np = img_np.astype(out_type)
-        result.append(img_np)
-    if len(result) == 1:
-        result = result[0]
-    return result
 
 
 def scandir(dir_path, suffix=None, recursive=False, full_path=False):
@@ -223,6 +92,33 @@ def scandir(dir_path, suffix=None, recursive=False, full_path=False):
                     continue
 
     return _scandir(dir_path, suffix=suffix, recursive=recursive)
+
+
+def check_resume(opt, resume_iter):
+    """Check resume states and pretrain_network paths.
+
+    Args:
+        opt (dict): Options.
+        resume_iter (int): Resume iteration.
+    """
+    logger = get_root_logger()
+    if opt['path']['resume_state']:
+        # get all the networks
+        networks = [key for key in opt.keys() if key.startwith('network_')]
+        flag_pretrain = False
+        for network in networks:
+            if opt['path'].get(f'pretrain_{network}') is not None:
+                flag_pretrain = True
+        if flag_pretrain:
+            logger.warning(
+                'pretrain_network path will be ignored during resuming.')
+        # set pretrained model paths
+        for network in networks:
+            name = f'pretrain_{network}'
+            basename = network.replace('network_', '')
+            opt['path'][name] = osp.join(opt['path']['models'],
+                                         f'net_{basename}_{resume_iter}.pth')
+            logger.info(f"Set {name} to {opt['path'][name]}")
 
 
 class ProgressBar(object):
