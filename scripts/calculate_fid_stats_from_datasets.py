e4ebae87008bd77978c7e400b2adae017314948e
Xintao <wxt1994@126.com>
2020-10-18 17:40:37 +0800

    add fid metric

---
+++ scripts/calculate_fid_stats_from_datasets.py
@@ -0,0 +1,72 @@
+import argparse
+import math
+import numpy as np
+import torch
+from torch.utils.data import DataLoader
+
+from basicsr.data import create_dataset
+from basicsr.metrics.fid import (extract_inception_features,
+                                 load_patched_inception_v3)
+
+
+def calculate_stats_from_dataset():
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--num_sample', type=int, default=50000)
+    parser.add_argument('--batch_size', type=int, default=64)
+    parser.add_argument('--size', type=int, default=512)
+    parser.add_argument('--dataroot', type=str, default='datasets/ffhq')
+    args = parser.parse_args()
+
+    # inception model
+    inception = load_patched_inception_v3(device)
+
+    # create dataset
+    opt = {}
+    opt['name'] = 'FFHQ'
+    opt['type'] = 'FFHQDataset'
+    opt['dataroot_gt'] = f'datasets/ffhq/ffhq_{args.size}.lmdb'
+    opt['io_backend'] = dict(type='lmdb')
+    opt['use_hflip'] = False
+    opt['mean'] = [0.5, 0.5, 0.5]
+    opt['std'] = [0.5, 0.5, 0.5]
+    dataset = create_dataset(opt)
+
+    # create dataloader
+    data_loader = DataLoader(
+        dataset=dataset,
+        batch_size=args.batch_size,
+        shuffle=False,
+        num_workers=4,
+        sampler=None,
+        drop_last=False)
+    total_batch = math.ceil(args.num_sample / args.batch_size)
+
+    def data_generator(data_loader, total_batch):
+        for idx, data in enumerate(data_loader):
+            if idx >= total_batch:
+                break
+            else:
+                yield data['gt']
+
+    features = extract_inception_features(
+        data_generator(data_loader, total_batch), inception, total_batch,
+        device)
+    features = features.numpy()
+    total_len = features.shape[0]
+    features = features[:args.num_sample]
+    print(f'Extracted {total_len} features, '
+          f'use the first {features.shape[0]} features to calculate stats.')
+    mean = np.mean(features, 0)
+    cov = np.cov(features, rowvar=False)
+
+    save_path = f'inception_{opt["name"]}_{args.size}.pth'
+    torch.save(
+        dict(name=opt['name'], size=args.size, mean=mean, cov=cov),
+        save_path,
+        _use_new_zipfile_serialization=False)
+
+
+if __name__ == '__main__':
+    calculate_stats_from_dataset()
---
+++ scripts/calculate_stylegan2_fid.py
@@ -0,0 +1,79 @@
+import argparse
+import math
+import numpy as np
+import torch
+from torch import nn
+
+from basicsr.metrics.fid import (calculate_fid, extract_inception_features,
+                                 load_patched_inception_v3)
+from basicsr.models.archs.stylegan2_arch import StyleGAN2Generator
+
+
+def calculate_stylegan2_fid():
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        'ckpt', type=str, help='Path to the stylegan2 checkpoint.')
+    parser.add_argument(
+        'fid_stats', type=str, help='Path to the dataset fid statistics.')
+    parser.add_argument('--size', type=int, default=256)
+    parser.add_argument('--channel_multiplier', type=int, default=2)
+    parser.add_argument('--batch_size', type=int, default=64)
+    parser.add_argument('--num_sample', type=int, default=50000)
+    parser.add_argument('--truncation', type=float, default=1)
+    parser.add_argument('--truncation_mean', type=int, default=4096)
+    args = parser.parse_args()
+
+    # create stylegan2 model
+    generator = StyleGAN2Generator(
+        out_size=args.size,
+        num_style_feat=512,
+        num_mlp=8,
+        channel_multiplier=args.channel_multiplier,
+        resample_kernel=(1, 3, 3, 1))
+    generator.load_state_dict(torch.load(args.ckpt)['params_ema'])
+    generator = nn.DataParallel(generator).eval().to(device)
+
+    if args.truncation < 1:
+        with torch.no_grad():
+            truncation_latent = generator.mean_latent(args.truncation_mean)
+    else:
+        truncation_latent = None
+
+    # inception model
+    inception = load_patched_inception_v3(device)
+
+    total_batch = math.ceil(args.num_sample / args.batch_size)
+
+    def sample_generator(total_batch):
+        for i in range(total_batch):
+            with torch.no_grad():
+                latent = torch.randn(args.batch_size, 512, device=device)
+                samples, _ = generator([latent],
+                                       truncation=args.truncation,
+                                       truncation_latent=truncation_latent)
+            yield samples
+
+    features = extract_inception_features(
+        sample_generator(total_batch), inception, total_batch, device)
+    features = features.numpy()
+    total_len = features.shape[0]
+    features = features[:args.num_sample]
+    print(f'Extracted {total_len} features, '
+          f'use the first {features.shape[0]} features to calculate stats.')
+    sample_mean = np.mean(features, 0)
+    sample_cov = np.cov(features, rowvar=False)
+
+    # load the dataset stats
+    stats = torch.load(args.fid_stats)
+    real_mean = stats['mean']
+    real_cov = stats['cov']
+
+    # calculate FID metric
+    fid = calculate_fid(sample_mean, sample_cov, real_mean, real_cov)
+    print('fid:', fid)
+
+
+if __name__ == '__main__':
+    calculate_stylegan2_fid()
