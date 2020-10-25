1c30e131ad1f992e8688817cfbc7a1b568cfbd1b
ZLI <zli@mail.com>
2020-10-25 22:49:28 +0100

    Fix path string in regroup_reds_dataset

--- scripts/regroup_reds_dataset.py
+++ scripts/regroup_reds_dataset.py
@@ -18,8 +18,9 @@ def regroup_reds_dataset(train_path, val_path):
     # move the validation data to the train folder
     val_folders = glob.glob(os.path.join(val_path, '*'))
     for folder in val_folders:
-        new_folder_idx = int(folder.split(' / ')[-1]) + 240
-        os.system(f'cp -r {folder} {os.path.join(train_path, new_folder_idx)}')
+        new_folder_idx = int(folder.split('/')[-1]) + 240
+        os.system(
+            f'cp -r {folder} {os.path.join(train_path, str(new_folder_idx))}')
 
 
 if __name__ == '__main__':
