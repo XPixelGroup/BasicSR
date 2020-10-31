c26dd943191a3c2528e1b924c366acb221ead86f
Mingyan Zhu <hiterzmy@gmail.com>
2020-10-31 14:43:36 +0800

    Update video_base_model.py

--- basicsr/models/video_base_model.py
+++ basicsr/models/video_base_model.py
@@ -34,9 +34,10 @@ class VideoBaseModel(SRModel):
                     len(self.opt['val']['metrics']),
                     dtype=torch.float32,
                     device='cuda')
+        rank, world_size = get_dist_info()
+        if with_metrics:
             for _, tensor in self.metric_results.items():
                 tensor.zero_()
-        rank, world_size = get_dist_info()
         # record all frames (border and center frames)
         if rank == 0:
             pbar = tqdm(total=len(dataset), unit='frame')
