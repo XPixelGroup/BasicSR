0aff099b04523b354e259f8f8aa6939211f3bfc6
wwhio <wangwenhao@live.com>
2021-03-13 17:20:18 +0800

    add docs for `dist_train.sh` script

--- scripts/dist_train.sh
+++ scripts/dist_train.sh
@@ -1,10 +1,15 @@
 #!/usr/bin/env bash
 
-# useage
+# usage
 
 GPUS=$1
 CONFIG=$2
 PORT=${PORT:-4321}
+
+if [ $# -le 3 ] ;then
+    echo "usage:"
+    echo "./scripts/dist_train.sh [number of gpu] [path to option file]"
+fi
 
 PYTHONPATH="$(dirname $0)/..:${PYTHONPATH}" \
 python -m torch.distributed.launch --nproc_per_node=$GPUS --master_port=$PORT \
