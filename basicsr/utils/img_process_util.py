de9a6cb5e96d96159325d7cc9ac27e57b617efc0
Xintao <wxt1994@126.com>
2021-07-23 12:42:45 +0800

    mv filter2D in img_process_util

--- basicsr/utils/img_process_util.py
+++ basicsr/utils/img_process_util.py
@@ -1,8 +1,34 @@
 import cv2
 import numpy as np
 import torch
+from torch.nn import functional as F
 
-from basicsr.data.data_util import filter2D
+
+def filter2D(img, kernel):
+    """PyTorch version of cv2.filter2D
+
+    Args:
+        img (Tensor): (b, c, h, w)
+        kernel (Tensor): (b, k, k)
+    """
+    k = kernel.size(-1)
+    b, c, h, w = img.size()
+    if k % 2 == 1:
+        img = F.pad(img, (k // 2, k // 2, k // 2, k // 2), mode='reflect')
+    else:
+        raise ValueError('Wrong kernel size')
+
+    ph, pw = img.size()[-2:]
+
+    if kernel.size(0) == 1:
+        # apply the same kenrel to all batch images
+        img = img.view(b * c, 1, ph, pw)
+        kernel = kernel.view(1, 1, k, k)
+        return F.conv2d(img, kernel, padding=0).view(b, c, h, w)
+    else:
+        img = img.view(1, b * c, ph, pw)
+        kernel = kernel.view(b, 1, k, k).repeat(1, c, 1, 1).view(b * c, 1, k, k)
+        return F.conv2d(img, kernel, groups=b * c).view(b, c, h, w)
 
 
 def usm_sharp(img, weight=0.5, radius=50, threshold=10):
