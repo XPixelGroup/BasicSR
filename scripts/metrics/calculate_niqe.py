1ca627d332cbb35676357a20fdb7e556de339f25
Xintao <wxt1994@126.com>
2021-08-09 20:58:57 +0800

    minor: niqe ignore RuntimeWarning

--- scripts/metrics/calculate_niqe.py
+++ scripts/metrics/calculate_niqe.py
@@ -1,6 +1,7 @@
 import argparse
 import cv2
 import os
+import warnings
 
 from basicsr.metrics import calculate_niqe
 from basicsr.utils import scandir
@@ -15,13 +16,13 @@ def main(args):
         basename, ext = os.path.splitext(os.path.basename(img_path))
         img = cv2.imread(img_path, cv2.IMREAD_UNCHANGED)
 
-        # calculate PSNR and SSIM
-        niqe_score = calculate_niqe(img, args.crop_border, input_order='HWC', convert_to='y')
+        with warnings.catch_warnings():
+            warnings.simplefilter('ignore', category=RuntimeWarning)
+            niqe_score = calculate_niqe(img, args.crop_border, input_order='HWC', convert_to='y')
         print(f'{i+1:3d}: {basename:25}. \tNIQE: {niqe_score:.6f}')
         niqe_all.append(niqe_score)
 
     print(args.input)
-
     print(f'Average: NIQE: {sum(niqe_all) / len(niqe_all):.6f}')
 
 
