f6b48e6755dd9ea57d848c30ed570fed77a29152
Pedro Cuenca <pedro@latenitesoft.com>
2022-02-21 17:25:17 +0000

    Models can be logged as W&B artifacts.
    Upload is controlled by a new option `logger`/`wandb`/`log_model`. When
    enabled, model files are uploaded to W&B as artifacts attached to the
    current run, at the frequency specified by `save_checkpoint_freq`.
    
    I'm using this feature for several purposes:
    - Centralized storage of models being trained in various locations.
    - Decoupling of training and testing. I usually upload models as training
    progresses, while a job running in a different computer downloads them
    and performs arbitrary metrics / evaluation tasks.

--- basicsr/utils/__init__.py
+++ basicsr/utils/__init__.py
@@ -2,7 +2,7 @@ from .diffjpeg import DiffJPEG
 from .file_client import FileClient
 from .img_process_util import USMSharp, usm_sharp
 from .img_util import crop_border, imfrombytes, img2tensor, imwrite, tensor2img
-from .logger import AvgTimer, MessageLogger, get_env_info, get_root_logger, init_tb_logger, init_wandb_logger
+from .logger import AvgTimer, MessageLogger, get_env_info, get_root_logger, init_tb_logger, init_wandb_logger, wandb_enabled, log_artifact
 from .misc import check_resume, get_time_str, make_exp_dirs, mkdir_and_rename, scandir, set_random_seed, sizeof_fmt
 
 __all__ = [
@@ -19,6 +19,8 @@ __all__ = [
     'AvgTimer',
     'init_tb_logger',
     'init_wandb_logger',
+    'wandb_enabled',
+    'log_artifact',
     'get_root_logger',
     'get_env_info',
     # misc.py
