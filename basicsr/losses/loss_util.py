a63d70eb6ea10fbc077157716a24f8576f0f4591
csjliang <liang27jie@gmail.com>
2022-04-28 21:59:22 +0800

    add weighting func

--- basicsr/losses/loss_util.py
+++ basicsr/losses/loss_util.py
@@ -93,3 +93,26 @@ def weighted_loss(loss_func):
         return loss
 
     return wrapper
+
+def get_local_weights(residual, ksize):
+
+    pad = (ksize - 1) // 2
+    residual_pad = F.pad(residual, pad=[pad, pad, pad, pad], mode='reflect')
+
+    unfolded_residual = residual_pad.unfold(2, ksize, 1).unfold(3, ksize, 1)
+    pixel_level_weight = torch.var(unfolded_residual, dim=(-1, -2), unbiased=True, keepdim=True).squeeze(-1).squeeze(-1)
+
+    return pixel_level_weight
+
+def get_refined_artifact_map(img_gt, img_output, img_ema, ksize):
+
+    residual_ema = torch.sum(torch.abs(img_gt - img_ema), 1, keepdim=True)
+    residual_SR = torch.sum(torch.abs(img_gt - img_output), 1, keepdim=True)
+
+    patch_level_weight = torch.var(residual_SR.clone(), dim=(-1, -2, -3), keepdim=True) ** (1/5)
+    pixel_level_weight = get_local_weights(residual_SR.clone(), ksize)
+    overall_weight = patch_level_weight * pixel_level_weight
+
+    overall_weight[residual_SR < residual_ema] = 0
+
+    return overall_weight
