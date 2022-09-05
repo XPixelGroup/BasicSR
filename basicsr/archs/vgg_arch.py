5a345c1dde1dcaf50947d352a4c295f866fbed90
Khoa D. Vo <khoa.supreme@gmail.com>
2022-09-05 12:41:22 +0700

    update deprecated VGG parameters

--- basicsr/archs/vgg_arch.py
+++ basicsr/archs/vgg_arch.py
@@ -101,11 +101,11 @@ class VGGFeatureExtractor(nn.Module):
                 max_idx = idx
 
         if os.path.exists(VGG_PRETRAIN_PATH):
-            vgg_net = getattr(vgg, vgg_type)(pretrained=False)
+            vgg_net = getattr(vgg, vgg_type)(weights=None)
             state_dict = torch.load(VGG_PRETRAIN_PATH, map_location=lambda storage, loc: storage)
             vgg_net.load_state_dict(state_dict)
         else:
-            vgg_net = getattr(vgg, vgg_type)(pretrained=True)
+            vgg_net = getattr(vgg, vgg_type)(weights=vgg.VGG19_Weights.DEFAULT)
 
         features = vgg_net.features[:max_idx + 1]
 
