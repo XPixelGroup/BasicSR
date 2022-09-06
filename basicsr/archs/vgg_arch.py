d1bdae5e6736124cc75b0ab1a8678e76f13ddbd0
Xintao <wxt1994@126.com>
2022-09-06 23:27:10 +0800

    revert vgg_arch

--- basicsr/archs/vgg_arch.py
+++ basicsr/archs/vgg_arch.py
@@ -101,11 +101,11 @@ class VGGFeatureExtractor(nn.Module):
                 max_idx = idx
 
         if os.path.exists(VGG_PRETRAIN_PATH):
-            vgg_net = getattr(vgg, vgg_type)(weights=None)
+            vgg_net = getattr(vgg, vgg_type)(pretrained=False)
             state_dict = torch.load(VGG_PRETRAIN_PATH, map_location=lambda storage, loc: storage)
             vgg_net.load_state_dict(state_dict)
         else:
-            vgg_net = getattr(vgg, vgg_type)(weights=vgg.VGG19_Weights.DEFAULT)
+            vgg_net = getattr(vgg, vgg_type)(pretrained=True)
 
         features = vgg_net.features[:max_idx + 1]
 
