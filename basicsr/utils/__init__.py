dd501350aee6c0b2af484150bfa7f7e8e1db0ab3
Xintao <wxt1994@126.com>
2022-09-08 13:07:22 +0800

    add yaml_load

--- basicsr/utils/__init__.py
+++ basicsr/utils/__init__.py
@@ -5,6 +5,7 @@ from .img_process_util import USMSharp, usm_sharp
 from .img_util import crop_border, imfrombytes, img2tensor, imwrite, tensor2img
 from .logger import AvgTimer, MessageLogger, get_env_info, get_root_logger, init_tb_logger, init_wandb_logger
 from .misc import check_resume, get_time_str, make_exp_dirs, mkdir_and_rename, scandir, set_random_seed, sizeof_fmt
+from .options import yaml_load
 
 __all__ = [
     #  color_util.py
@@ -40,5 +41,7 @@ __all__ = [
     'DiffJPEG',
     # img_process_util
     'USMSharp',
-    'usm_sharp'
+    'usm_sharp',
+    # options
+    'yaml_load'
 ]
