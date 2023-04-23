c968b524e7a9b8e08cafe52a6cbd7eb617cb4af5
purewind7 <757419920@qq.com>
2023-04-23 20:29:42 +0800

    Update paired_image_dataset.py
    fix normalize in dataset

--- basicsr/data/paired_image_dataset.py
+++ basicsr/data/paired_image_dataset.py
@@ -96,7 +96,7 @@ class PairedImageDataset(data.Dataset):
         # BGR to RGB, HWC to CHW, numpy to tensor
         img_gt, img_lq = img2tensor([img_gt, img_lq], bgr2rgb=True, float32=True)
         # normalize
-        if self.mean is not None or self.std is not None:
+        if self.mean is not None and self.std is not None:
             normalize(img_lq, self.mean, self.std, inplace=True)
             normalize(img_gt, self.mean, self.std, inplace=True)
 
